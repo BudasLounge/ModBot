ec4655a8ad18e8635e1e523693c6c516f3a1efe4
Graber <graber15@purdue.edu>
2019-12-17 16:19:27 -0500

    Bugfixing

--- modules/admin/commands/ping.js
+++ modules/admin/commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Sends a message back. Used to test if the bot is working.',
     syntax: 'ping [arbitrary argument for testing]'
-    num_args: 0,
+    num_args: 1,
     execute(message, args) {
         message.channel.send('Admin Pong.');
     }
