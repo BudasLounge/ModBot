b8842406d24833dc94dc498c9ba232b454579fba
Andrew C Graber <graber15@purdue.edu>
2020-01-14 14:34:01 -0500

    testing

--- modules/dnd/commands/ping.js
+++ modules/dnd/commands/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'ping',
     description: 'Ping!',
+    syntax: 'ping',
+    num_args: 0,
     execute(message, args) {
         message.channel.send('D&D Pong.');
     }
