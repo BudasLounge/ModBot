8905e8658a9717636977f494832a815cc7efb462
AnZaNaMa <recovery22306@gmail.com>
2020-01-15 11:01:37 -0500

    added modules command

---
+++ modules/core/commands/modules.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: 'modules',
+    description: 'Provides information about the modules ModBot has loaded. Optional second argument to filter modules. Default is \'all\'',
+    syntax: 'modules [enabled|disabled|all]',
+    num_args: 0,
+    execute(message, args, mod_handler) {
+        var output = '```';
+        var module_type = "all";
+        if(args.length > 1) {
+            module_type = args[1];
+        }
+
+        if(module_type == "enabled" || module_type == "all") {
+            output += "Enabled Modules:\n";
+            for(var current_module_name of Array.from(mod_handler.modules.keys())) {
+                var current_module = mod_handler.modules.get(current_module_name);
+                output += "  - " + current_module.config.name + "\n";
+            }
+        }
+
+        if(module_type == "all") {
+            output += "\n";
+        }
+
+        if(module_type == "disabled" || module_type == "all") {
+            output += "Disabled Modules:\n";
+            for(var current_module_name of Array.from(mod_handler.disabled_modules.keys())) {
+                var current_module = mod_handler.diabled_modules.get(current_module_name);
+                output += "  - " + current_module.config.name + "\n";
+            }
+        }
+
+        output += "```";
+        message.channel.send(output);
+    }
+};
