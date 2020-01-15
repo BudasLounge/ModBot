9efc681d2f5431a1e7768499b859f71417a561e4
Andrew C Graber <graber15@purdue.edu>
2020-01-15 13:23:53 -0500

    added better formatting for commands command

---
+++ modules/core/commands/reload.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+module.exports = {
+    name: 'reload',
+    description: 'Reloads all modules and their commands/config files.',
+    syntax: 'reload',
+    num_args: 0,
+    execute(message, args, mod_handler) {
+        var config = JSON.parse(fs.readFileSync(mod_handler.program_path + '/modbot.json'));
+        mod_handler.discover_modules(mod_handler.program_path + "/" + config.modules_folder);
+        mod_handler.discover_commands();
+
+        var num_commands = 0;
+        for(var current_module_name of Array.from(mod_handler.modules.keys())) {
+            var current_module = mod_handler.modules.get(current_module_name);
+            num_commands += current_module.commands.size;
+        }
+
+        message.channel.send("Reload Complete! Loaded " + mod_handler.modules.size + " modules and " + num_commands + " commands.");
+    }
+};
