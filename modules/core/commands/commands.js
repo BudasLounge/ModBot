8ae88466ff9bb26332e9009bede4440633365cb0
Andrew C Graber <graber15@purdue.edu>
2020-01-15 12:45:29 -0500

    added better formatting for commands command

--- modules/core/commands/commands.js
+++ modules/core/commands/commands.js
@@ -9,10 +9,43 @@ module.exports = {
             var module_name = args[1];
             if(mod_handler.modules.has(module_name)) {
                 var selected_module = mod_handler.modules.get(module_name);
-                output += "Commands from module '" + selected_module.config.display_name + "':\n";
+
+                var longest_syntax = "";
+                var longest_module_name = module_name;
                 for(var current_command_name of Array.from(selected_module.commands.keys())) {
                     var current_command = selected_module.commands.get(current_command_name);
-                    output += current_command.syntax + " | " + selected_module.config.display_name + " | " + current_command.description + "\n";
+                    if(current_command.syntax.length > longest_syntax) {
+                        longest_syntax = current_command.syntax;
+                    }
+                }
+
+                var desc_space = 134 - longest_syntax.length - longest_module_name.length;
+
+                for(var current_command_name of Array.from(selected_module.commands.keys())) {
+                    var current_command = selected_module.commands.get(current_command_name);
+                    
+                    output += current_command.syntax;
+                    if(current_command.syntax.length < longest_syntax.length) {
+                        for(var i=current_command.syntax.length; i < longest_syntax.length; i++) {
+                            output += " ";
+                        }
+                    }
+
+                    output += " | " + selected_module.config.display_name;
+                    if(selected_module.config.display_name.length < longest_module_name.length) {
+                        for(var i=selected_module.config.display_name.length; i < longest_module_name.length; i++) {
+                            output += " ";
+                        }
+                    }
+
+                    output += " | ";
+                    if(current_command.description.length > desc_space) {
+                        output += current_command.description.substring(0, desc_space - 3) + "...";
+                    } else {
+                        output += current_command.description;
+                    }
+
+                    output += "\n";
                 }
                 output += "```";
                 message.channel.send(output);
