b593949c8fbb85efcedae1c3f0d0333f0c2eb88e
Andrew C Graber <graber15@purdue.edu>
2020-01-15 12:16:40 -0500

    added commands command

--- modules/core/commands/commands.js
+++ modules/core/commands/commands.js
@@ -9,7 +9,7 @@ module.exports = {
             var module_name = args[1];
             if(mod_handler.modules.has(module_name)) {
                 var selected_module = mod_handler.modules.get(module_name);
-                output += "Commands from module '" + module_name + "':\n";
+                output += "Commands from module '" + selected_module.config.display_name + "':\n";
                 for(var current_command_name of Array.from(selected_module.commands.keys())) {
                     var current_command = selected_module.commands.get(current_command_name);
                     output += current_command.syntax + " | " + selected_module.config.display_name + " | " + current_command.description + "\n";
@@ -22,7 +22,7 @@ module.exports = {
         } else {
             for(var current_module_name of Array.from(mod_handler.modules.keys())) {
                 var current_module = mod_handler.modules.get(current_module_name);
-                output += "Commands from module '" + current_module_name + "':\n";
+                output += "Commands from module '" + current_module.config.display_name + "':\n";
                 for(var current_command_name of Array.from(current_module.commands.keys())) {
                     var current_command = current_module.commands.get(current_command_name);
                     output += current_command.syntax + " | " + current_module.config.display_name + " | " + current_command.description + "\n";
