94e958ca2ac865291c874b6f30b056560553755e
JR Buda <jrbuda01@gmail.com>
2020-01-21 23:06:03 -0500

    working on mc module

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -11,7 +11,7 @@ module.exports ={
         } catch(error){
             console.error(error);
         } 
-        getServerStatus(respServer.minecraft_servers[0].display_name, respServer.minecraft_servers[0].port, respServer.minecraft_servers[0].server_ip, message.channel);
+        getServerStatus(respServer.minecraft_servers[0].display_name, respServer.minecraft_servers[0].port, respServer.minecraft_servers[0].numeric_ip, message.channel);
     }
 };
 
