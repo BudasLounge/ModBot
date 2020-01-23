7e018078c6e8e21d49be8420a77e6922d2c40965
JR Buda <jrbuda01@gmail.com>
2020-01-23 15:42:02 -0500

    mc bot command rewrite

--- modules/minecraft/commands/get_ip.js
+++ modules/minecraft/commands/get_ip.js
@@ -5,7 +5,7 @@ module.exports = {
     num_args: 1,
     args_to_lower: true,
     execute(message, args, api) {
-       /* try{
+        try{
             respServer = await api.get("minecraft_server", {
                 short_name: args[1]
             });
@@ -22,6 +22,6 @@ module.exports = {
             }catch(error2){
                 console.error(error2);
             }
-        }*/
+        }
     }
 };
