dd16463354c7a78a51255bcf6cfb23aa25086d97
Joseph R Buda <jbuda@purdue.edu>
2020-01-24 16:16:07 -0500

    abot functions

--- modules/admin/events.js
+++ modules/admin/events.js
@@ -6,12 +6,13 @@ function onMessageReactionAdd(messageReaction, user) {
 
 async function onUserJoin(member){
     try{
-        respServer = await api.get("DiscordServer", {
+        respServer = await api.get("discord_server", {
             server_id: member.guild.id
         });
     }catch(error){
         console.error(error);
     }
+    console.log(respServer);
     if(respServer.discord_servers[0]){
         member.guild.channels.get(respServer.discord_servers[0].welcome_channel_id).send("<@" + member.id + "> "+respServer.discord_servers[0].welcome_message);
         member.addRole(respServer.discord_servers[0].default_role_id);
