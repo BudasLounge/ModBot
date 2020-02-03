069b77ab10b72c8816ee769708fb545b48c3f2cb
JR Buda <jrbuda01@gmail.com>
2020-02-03 14:55:15 -0500

    updatesl mc

--- modules/minecraft/commands/update_serverlist.js
+++ modules/minecraft/commands/update_serverlist.js
@@ -6,12 +6,12 @@ module.exports = {
     args_to_lower: true,
     async execute(message, args, api) {
         var respServer = await api.get("minecraft_server", {
-            short_name = args[1]
+            short_name: args[1]
         });
         if(!respServer[0]){
             message.channel.send("No server with that short_hand...checking display_name");
             respServer = await api.get("minecraft_server", {
-                sdisplay_name = args[1]
+                sdisplay_name: args[1]
             });
         }
         if(respServer[0]){
