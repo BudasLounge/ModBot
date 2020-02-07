5fc624911e6ba8b3bcf95f59c795e4a511b285cd
Buda <jbuda@purdue.edu>
2020-02-07 16:08:19 -0500

    creating add_server functionality

--- modules/minecraft/commands/add_server.js
+++ modules/minecraft/commands/add_server.js
@@ -5,16 +5,20 @@ module.exports = {
     num_args: 6,
     args_to_lower: true,
     async execute(message, args, api) {
+        console.log("in function");
         var respServer;
         try{
+            console.log("in try");
             respServer = await api.get("minecraft_server", {
                 server_ip: args[3]
             });
         } catch(error){
             console.error(error);
         } 
+        console.log("running if");
         if(!respServer.minecraft_servers[0]){
             message.channel.send("Found nothing!");
         }
+        console.log("exiting");
     }
 };
