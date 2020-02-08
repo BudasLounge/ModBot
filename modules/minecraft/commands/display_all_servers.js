b2d46a388c53d4ea1b26c4627f92b2e303c414f2
Buda <jbuda@purdue.edu>
2020-02-07 19:02:43 -0500

    add_server

--- modules/minecraft/commands/display_all_servers.js
+++ modules/minecraft/commands/display_all_servers.js
@@ -12,14 +12,13 @@ module.exports ={
             console.error(error);
         }
         console.log(respServer.minecraft_servers.length + " servers found...");
-        var output;
+        var output = "";
         for(var i = 0;i<respServer.minecraft_servers.length;i++){
             output += "------------------------------\n";
             output += respServer.minecraft_servers[i].display_name + " server info:\n";
             output += "short name: " + respServer.minecraft_servers[i].short_name + "\n";
             output += "server ip: " + respServer.minecraft_servers[i].server_ip + "\n";
             output += "numeric ip: " + respServer.minecraft_servers[i].numeric_ip + ":" + respServer.minecraft_servers[i].port + "\n";
-            output += "------------------------------\n";
         }
         message.channel.send(output);
     }
