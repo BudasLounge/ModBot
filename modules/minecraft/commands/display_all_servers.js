50565efa5974e7907e9cb7e20509c4e1283f0ca3
Buda <jbuda@purdue.edu>
2020-02-13 19:13:08 -0500

    adding aesthetics

--- modules/minecraft/commands/display_all_servers.js
+++ modules/minecraft/commands/display_all_servers.js
@@ -4,6 +4,7 @@ module.exports ={
     syntax: 'showAll',
     num_args: 0,
     async execute(message, args, api){
+        const Discord = require('discord.js');
         console.log(">>display_all_servers");
         try{
             respServer = await api.get("minecraft_server", {
@@ -13,7 +14,7 @@ module.exports ={
             console.error(error);
         }
         console.log(respServer.minecraft_servers.length + " servers found...");
-        var output = "```";
+        //var output = "```";
         for(var i = 0;i<respServer.minecraft_servers.length;i++){
             output += "------------------------------\n";
             output += respServer.minecraft_servers[i].display_name + " server info:\n";
@@ -21,8 +22,11 @@ module.exports ={
             output += "server ip: " + respServer.minecraft_servers[i].server_ip + "\n";
             output += "numeric ip: " + respServer.minecraft_servers[i].numeric_ip + ":" + respServer.minecraft_servers[i].port + "\n";
         }
-        output += "------------------------------```";
-        message.channel.send(output);
+        //output += "------------------------------```";
+        const ListEmbed = new Discord.RichEmbed()
+        .setTitle("List of all minecraft servers: ")
+        .setDescription(output);
+        message.channel.send(ListEmbed);
         console.log("<<display_all_servers");
     }
 };
