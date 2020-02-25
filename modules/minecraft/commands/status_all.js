f5e3e361c3ac5181707e3360c6c10a5733f6364a
JR Buda <jrbuda01@gmail.com>
2020-02-25 13:57:03 -0500

    show all server status

--- modules/minecraft/commands/status_all.js
+++ modules/minecraft/commands/status_all.js
@@ -5,7 +5,7 @@ module.exports ={
     num_args: 0,
     async execute(message, args, api){
         const Discord = require('discord.js');
-        console.log(">>display_all_servers");
+        console.log(">>display_all_servers_status");
         try{
             respServer = await api.get("minecraft_server", {
                 
@@ -19,13 +19,33 @@ module.exports ={
         .setTitle("List of all minecraft servers: ");
         for(var i = 0;i<respServer.minecraft_servers.length;i++){
             var nextItem = "";
-            nextItem += "short name: " + respServer.minecraft_servers[i].short_name + "\n";
-            nextItem += "server ip: " + respServer.minecraft_servers[i].server_ip + "\n";
-            nextItem += "numeric ip: " + respServer.minecraft_servers[i].numeric_ip + ":" + respServer.minecraft_servers[i].port;
-
-            ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", nextItem);
+            nextItem += getServerStatus(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip)
         }
         message.channel.send(ListEmbed);
-        console.log("<<display_all_servers");
+        console.log("<<display_all_servers_status");
     }
 };
+
+
+function getServerStatus(server, port, ip, channel){
+    const request = require('request');
+    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
+    request(url, function(err, response, body) {
+        if(err) {
+            console.log(err);
+            return message.reply('Error getting server status...');
+        }
+        body = JSON.parse(body);
+        var status = '*'+server+' server is currently offline*';
+        if(body.online) {
+            status = '**'+server+'** server is **online**  -  ';
+            if(body.players.now) {
+                status += '**' + body.players.now + '** people are playing!';
+            } else {
+                status += '*Nobody is playing!*';
+            }
+        }
+        console.log("Returning message: "+status);
+        return status;
+    });
+}
