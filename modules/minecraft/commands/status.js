181ce400c73016d4a8f2fa0dde7a0b2c74bb0be9
Buda <jbuda@purdue.edu>
2020-04-15 16:33:56 -0400

    updating status command

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -14,7 +14,12 @@ module.exports ={
             console.error(error);
         } 
         console.log(respServer);
-        getServerStatus(respServer.minecraft_servers[0].display_name, respServer.minecraft_servers[0].port, respServer.minecraft_servers[0].numeric_ip, message.channel);
+        var item = await getServerState(respServer.minecraft_servers[0].display_name, respServer.minecraft_servers[0].port, respServer.minecraft_servers[0].numeric_ip, message.channel);
+        const ListEmbed = new Discord.RichEmbed()
+        .setColor("#f92f03")
+        .setTitle("List of all minecraft servers: ");
+        ListEmbed.addField(respServer.minecraft_servers[0].display_name + " server info:", item);
+        message.channel.send(ListEmbed);
     }
 };
 
@@ -39,4 +44,22 @@ function getServerStatus(server, port, ip, channel){
         console.log("Returning message: "+status);
         channel.send(status);
     });
+}
+
+async function getServerState(server, port, ip){
+    var axios = require('axios');
+    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
+    var response = await axios.get(url);
+    response = response.data;
+    var status = '*'+server+' server is currently offline*'
+    if(response.online) {
+        status = '**'+server+'** server is **online**  -  ';
+        if(response.players.now) {
+            status += '**' + response.players.now + '** people are playing!';
+        } else {
+            status += '*Nobody is playing!*';
+        }
+    }
+    console.log("Returning message: "+status);
+    return status;
 }
