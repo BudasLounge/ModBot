9fbc7d28ceb1fbc07a1696877badfe45c63f3351
Buda <jbuda@purdue.edu>
2020-04-20 17:25:33 -0400

    working on commands to update server information from discord

--- modules/minecraft/commands/update_serverlist.js
+++ modules/minecraft/commands/update_serverlist.js
@@ -5,6 +5,7 @@ module.exports = {
     num_args: 3,
     args_to_lower: true,
     async execute(message, args, api) {
+        const Discord = require('discord.js');
         var respServer;
         try{
             respServer = await api.get("minecraft_server", {
@@ -26,12 +27,33 @@ module.exports = {
         if(respServer.minecraft_servers[0]){
             message.channel.send("Found one!");
             try{
-                var argument2 = args[2];
                 var data = {short_name: respServer.minecraft_servers[0].short_name};
                 data[args[2]] = args[3];
                 var respUpdate = await api.put("minecraft_server", data);
-                console.log(respUpdate);
-                console.log(respServer.minecraft_servers[0].argument2 + " was changed to " + respUpdate.minecraft_server.argument2);
+                if(respUpdate.ok == true){
+                    const ListEmbed = new Discord.RichEmbed()
+                    .setColor("#f92f03")
+                    .setTitle("Here's what changed: ");
+                    var changedInfo = "";
+                    changedInfo += "short_name: " + respServer.minecraft_servers[0].short_name + "\n";
+                    changedInfo += "display_name: " + respServer.minecraft_servers[0].display_name + "\n";
+                    changedInfo += "server_ip + port: " + respServer.minecraft_servers[0].server_ip + "\n";
+                    changedInfo += "numeric_ip: " + respServer.minecraft_servers[0].numeric_ip + ":" + respServer.minecraft_servers[0].port + "\n";
+                    changedInfo += "status_api_port: " + respServer.minecraft_servers[0].status_api_port + "\n";
+                    changedInfo += "mc_version: " + respServer.minecraft_servers[0].mc_version + "\n";
+                    changedInfo += "pack_version: " + respServer.minecraft_servers[0].pack_version;
+                    changedInfo += "\nvvvvv has been changed to vvvvv\n";
+                    changedInfo += "short_name: " + respUpdate.minecraft_server.short_name + "\n";
+                    changedInfo += "display_name: " + respUpdate.minecraft_server.display_name + "\n";
+                    changedInfo += "server_ip + port: " + respUpdate.minecraft_server.server_ip + "\n";
+                    changedInfo += "numeric_ip: " + respUpdate.minecraft_server.numeric_ip + ":" + respUpdate.minecraft_server.port + "\n";
+                    changedInfo += "status_api_port: " + respUpdate.minecraft_server.status_api_port + "\n";
+                    changedInfo += "mc_version: " + respUpdate.minecraft_server.mc_version + "\n";
+                    changedInfo += "pack_version: " + respUpdate.minecraft_server.pack_version;
+
+                    ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", changedInfo);
+                    message.channel.send(ListEmbed);
+                }
             } catch(err3){
                 console.error(err3);
             }
