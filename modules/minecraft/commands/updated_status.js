e060771e8a899c946a6b5d68c6b1138a1c18f221
Buda <jbuda@purdue.edu>
2020-04-22 17:55:41 -0400

    found a new way to ping without an API

--- modules/minecraft/commands/updated_status.js
+++ modules/minecraft/commands/updated_status.js
@@ -17,21 +17,21 @@ module.exports ={
         } 
         var status = mcping(respServer.minecraft_servers[0].server_ip, respServer.minecraft_servers[0].port, function(err, res) {
             if (!(typeof err === 'undefined' || err === null)) {
-                var ServerStatus = (server + ' server is currently offline');
+                var ServerStatus = ' server is currently offline';
                 console.log(ServerStatus);
-                //message.channel.send(ServerStatus);
+                message.channel.send(ServerStatus);
                 return ServerStatus;
             }
             else if (typeof res.players.online === 0) { 
-                var ServerStatusNoOne = (server + ' server is **online**  -  *Nobody is playing!*'); 
+                var ServerStatusNoOne = ' server is **online**  -  *Nobody is playing!*'; 
                 console.log(ServerStatusNoOne);
-                //message.channel.send(ServerStatusNoOne);
+                message.channel.send(ServerStatusNoOne);
                 return ServerStatusNoOne;
             }
             else if (!(typeof res.players.online === 0)) { 
-                var ServerStatusSomeone =  (server + ' server is **online**  -  **'+res.players.online+'** people are playing!'); 
+                var ServerStatusSomeone =  ' server is **online**  -  **'+res.players.online+'** people are playing!'; 
                 console.log(ServerStatusSomeone); 
-                //message.channel.send(ServerStatusSomeone);
+                message.channel.send(ServerStatusSomeone);
                 return ServerStatusSomeone;
             }
         })
