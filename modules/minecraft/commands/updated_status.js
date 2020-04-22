d42534df7933ecc7b15cf2b993f04fcccae65f92
Buda <jbuda@purdue.edu>
2020-04-22 17:21:14 -0400

    found a new way to ping without an API

---
+++ modules/minecraft/commands/updated_status.js
@@ -0,0 +1,45 @@
+module.exports ={
+    name: 'up_status',
+    description: 'Finds the status of a minecraft server',
+    syntax: 'up_status [name of server]',
+    num_args: 1,
+    args_to_lower: true,
+    async execute(message, args, api){
+        const Discord = require('discord.js');
+        var respServer;
+        try{
+            respServer = await api.get("minecraft_server", {
+                short_name: args[1]
+            });
+        } catch(error){
+            console.error(error);
+        } 
+        var status = mcping(respServer.minecraft_servers[0].server_ip, respServer.minecraft_servers[0].port, function(err, res) {
+            if (!(typeof err === 'undefined' || err === null)) {
+                ServerStatus += ' server is currently offline';
+                return ServerStatus;
+            }
+            if (typeof res.players.sample === 'undefined') { ServerStatus += ' server is **online**  -  *Nobody is playing!*'; return ServerStatus;}
+            if (!(typeof res.players.sample === 'undefined')) { ServerStatus +=  ' server is **online**  -  **'+res.players.online+'** people are playing!'; return ServerStatus; }
+        })
+        message.channel.send(status);
+    }
+};
+
+async function getServerState(server, port, ip){
+    var axios = require('axios');
+    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
+    var response = await axios.get(url);
+    response = response.data;
+    var status = '*'+server+' server is currently offline*';
+    if(response.online) {
+        status = '**'+server+'** server is **online**  -  ';
+        if(response.players.now) {
+            status += '**' + response.players.now + '** people are playing!';
+        } else {
+            status += '*Nobody is playing!*';
+        }
+    }
+    console.log("Returning message: "+status);
+    return status;
+}
