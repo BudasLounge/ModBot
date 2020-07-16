448d5e66c138f988190c667ba608a0b436b039d8
JR Buda <jrbuda01@gmail.com>
2020-07-16 16:25:10 -0400

    dnd commands start

--- modules/dnd/commands/start_campaign.js
+++ modules/dnd/commands/start_campaign.js
@@ -20,7 +20,7 @@ module.exports = {
         const filter1 = response1 => {
         return response1.author.id === Authorid;
         }
-        message.channel.send("What is the module you are using?").then(() => {
+        message.channel.send("<@" + message.member.id + "> "+"What is the module you are using?").then(() => {
         message.channel.awaitMessages(filter1, { max: 1 })
         .then(collected1 => {
             
@@ -30,7 +30,7 @@ module.exports = {
         const filter2 = response2 => {
         return response2.author.id === Authorid;
         }
-        message.channel.send("What is the start date? (Format YYYY-MM-DD)").then(() => {
+        message.channel.send("<@" + message.member.id + "> "+"What is the start date? (Format YYYY-MM-DD)").then(() => {
         message.channel.awaitMessages(filter2, { max: 1 })
         .then(collected2 => {
             
@@ -40,14 +40,14 @@ module.exports = {
         const filter3 = response3 => {
         return response3.author.id === Authorid;
         }
-        message.channel.send("How many days in between sessions? (enter only the numeric value)").then(() => {
+        message.channel.send("<@" + message.member.id + "> "+"How many days in between sessions? (enter only the numeric value)").then(() => {
         message.channel.awaitMessages(filter3, { max: 1 })
         .then(collected3 => {
             
         const response3 = collected3.first();
             schedule_type = response3.content;
 
-            message.channel.send("Here is what you entered:\n" + module + "\n" + start_date + "\n" + schedule_type);
+            message.channel.send("<@" + message.member.id + "> "+"Here is what you entered:\n" + module + "\n" + start_date + "\n" + schedule_type);
         });
         });
         });
