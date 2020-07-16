b60caf2a2c39a07456e7217b7c5ebd1f1c329c27
JR Buda <jrbuda01@gmail.com>
2020-07-16 14:23:47 -0400

    dnd commands start

--- modules/dnd/commands/start_campaign.js
+++ modules/dnd/commands/start_campaign.js
@@ -20,30 +20,30 @@ module.exports = {
         const filter1 = response1 => {
         return response1.author.id === Authorid;
         }
-
+        message.channel.send("What is the module you are using?");
         message.channel.awaitMessages(filter1, { max: 1 })
         .then(collected1 => {
-            message.channel.send("What is the module you are using?");
+            
         const response1 = collected1.first();
             module = response1.content;
 
         const filter2 = response2 => {
         return response2.author.id === Authorid;
         }
-
+        message.channel.send("What is the start date? (Format YYYY-MM-DD)");
         message.channel.awaitMessages(filter2, { max: 1 })
         .then(collected2 => {
-            message.channel.send("What is the start date? (Format YYYY-MM-DD)");
+            
         const response2 = collected2.first();
         start_date = response2.content;
 
         const filter3 = response3 => {
         return response3.author.id === Authorid;
         }
-
+        message.channel.send("How many days in between sessions? (enter only the numeric value)");
         message.channel.awaitMessages(filter3, { max: 1 })
         .then(collected3 => {
-            message.channel.send("How many days in between sessions? (enter only the numeric value)");
+            
         const response3 = collected3.first();
             schedule_type = response3.content;
 
