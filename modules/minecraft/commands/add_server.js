9f8b91cb530a38034450c2d5813a88dcb887811f
JR Buda <jrbuda01@gmail.com>
2020-07-16 00:33:31 -0400

    fixing updatesl

--- modules/minecraft/commands/add_server.js
+++ modules/minecraft/commands/add_server.js
@@ -6,36 +6,41 @@ module.exports = {
     args_to_lower: true,
     async execute(message, args, api) {
         console.log(">>add_server");
-        var respServer;
-        try{
-            console.log("in try");
-            respServer = await api.get("minecraft_server", {
-                server_ip: args[3]
-            });
-        } catch(error){
-            console.error(error);
-        } 
-        if(!respServer.minecraft_servers[0]){
-            message.channel.send("Adding server " + args[1] + " to the database, here we goooooooo");
+        if(message.member.roles.has("586313447965327365")){
+            var respServer;
             try{
-                await api.post("minecraft_server", {
-                    display_name: args[1],
-                    short_name: args[2],
-                    server_ip: args[3],
-                    port: args[4],
-                    status_api_port: args[5],
-                    numeric_ip: args[6],
-                    mc_version: args[7],
-                    pack_version: args[8]
+                console.log("in try");
+                respServer = await api.get("minecraft_server", {
+                    server_ip: args[3]
                 });
-            }catch(err){
-                console.error(err);
-                message.channel.send("I hit a snag...");
+            } catch(error){
+                console.error(error);
+            } 
+            if(!respServer.minecraft_servers[0]){
+                message.channel.send("Adding server " + args[1] + " to the database, here we goooooooo");
+                try{
+                    await api.post("minecraft_server", {
+                        display_name: args[1],
+                        short_name: args[2],
+                        server_ip: args[3],
+                        port: args[4],
+                        status_api_port: args[5],
+                        numeric_ip: args[6],
+                        mc_version: args[7],
+                        pack_version: args[8]
+                    });
+                }catch(err){
+                    console.error(err);
+                    message.channel.send("I hit a snag...");
+                }
             }
+            else{
+                message.channel.send("I found a server with that server_ip already, try again");
+            }
+            console.log("<<add_server");
+        }else{
+            message.channel.send("You don't have permission to use that command!");
         }
-        else{
-            message.channel.send("I found a server with that server_ip already, try again");
-        }
-        console.log("<<add_server");
     }
+
 };
