b648df9714a331aa2381465efaff6faa59e53a25
JR Buda <jrbuda01@gmail.com>
2020-07-17 00:04:36 -0400

    dnd commands

--- modules/dnd/commands/start_campaign.js
+++ modules/dnd/commands/start_campaign.js
@@ -86,7 +86,7 @@ module.exports = {
             
 
         message.guild.createChannel(category_name, "category");
-        let category = message.guild.channels.cache.find(c => (c.name == category_name && c.type == "category"));
+        let category = message.guild.channels.cache.find(c => c.name == category_name);
         for(var i = 0;i < text_amount;i++){
             const filter8 = response8 => {
                 return response8.author.id === Authorid;
