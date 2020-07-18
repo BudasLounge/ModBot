b16e8fd09a1e6d8a7ff2f343933aecaa84a03308
JR Buda <jrbuda01@gmail.com>
2020-07-17 20:32:05 -0400

    sprucing up mc commands

--- modules/minecraft/commands/status_all.js
+++ modules/minecraft/commands/status_all.js
@@ -22,7 +22,7 @@ module.exports ={
         for(var i = 0;i<respServer.minecraft_servers.length;i++){
             var nextItem = "";
             //nextItem += getServerStatus(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip);
-            nextItem += await getServerState(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip);
+            nextItem += await getServerState(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip, respServer.minecraft_servers[i].status_api_port);
             //console.log(nextItem);
             ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", nextItem);
         }
@@ -32,7 +32,7 @@ module.exports ={
     }
 };
 
-async function getServerState(server, port, ip){
+/*async function getServerState(server, port, ip){
     var axios = require('axios');
     var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
     var response = await axios.get(url);
@@ -45,6 +45,39 @@ async function getServerState(server, port, ip){
         } else {
             status += '*Nobody is playing!*';
         }
+    }
+    console.log("Returning message: "+status);
+    return status;
+}*/
+async function getServerState(server, port, ip, status_api_port){
+    var axios = require('axios');
+    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
+    var response = await axios.get(url);
+    response = response.data;
+    var status = '*'+server+' server is currently offline*';
+    try{
+        if(response.online) {
+            status = '**'+server+'** server is **online**  -  ';
+            if(response.players.now) {
+                status += '**' + response.players.now + '** people are playing!';
+                if(status_api_port != "none"){
+                    status += "\nPlayers: ";
+                    var respPlayers = await axios.get("http://192.168.1.2:" + status_api_port + "/player-list", {});
+                    console.log("returned from api:\n" + respPlayers.data.players);
+                    if(respPlayers.data.players.length == 0) {
+                        status += ".";
+                    } else {
+                        for(var player of respPlayers.data.players) {
+                            status += "\n  - " + player.username;
+                        }
+                    }
+                }
+            } else {
+                status += '*Nobody is playing!*';
+            }
+        }
+    }catch(err){
+        console.error(err);
     }
     console.log("Returning message: "+status);
     return status;
