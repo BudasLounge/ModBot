012b9140d6f0c9314469b9c46951e46f3d72f890
Andrew Graber <agraber@belltechlogix.com>
2020-08-12 09:43:03 -0400

    Testing state system

---
+++ modules/core/commands/statetest.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'statetest',
+    description: 'Helps test the saved-state system!',
+    syntax: 'statetest',
+    num_args: 0,
+    args_to_lower: true,
+    has_state: true,
+    execute(message, args, api, state, mod_handler) {
+      this.logger.info(state);
+
+      message.channel.send("Admin Pong!");
+    }
+};
