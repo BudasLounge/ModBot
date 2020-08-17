c80ea286a34dc30a04ed9b6a4a47a72906536049
JR Buda <jrbuda01@gmail.com>
2020-08-17 00:29:37 -0400

    league commands

--- modules/league/commands/update_champion.js
+++ modules/league/commands/update_champion.js
@@ -17,24 +17,28 @@ module.exports = {
             this.logger.error(error.response);
         }
         if(respChamps.league_champions[0]){
-            var data = {name: respChamps.league_champions[0].name};
-            data[args[2]] = args[3];
-            var respUpdate = await api.put("league_champion" , data);
-            if(respUpdate.ok == true){
-                const ListEmbed = new Discord.RichEmbed()
-                    .setColor("#f92f03")
-                    .setTitle("Here's what changed: ");
-                var changedInfo = "";
-                changedInfo += "name: " + respChamps.league_champions[0].name + "\n";
-                changedInfo += "role_primary: " + respChamps.league_champions[0].role_primary + "\n";
-                changedInfo += "role_secondary: " + respChamps.league_champions[0].role_secondary + "\n";
-                changedInfo += "\n\nvvvvv has been changed to vvvvv\n\n";
-                changedInfo += "name: " + respUpdate.league_champions[0].name + "\n";
-                changedInfo += "role_primary: " + respUpdate.league_champions[0].role_primary + "\n";
-                changedInfo += "role_secondary: " + respUpdate.league_champions[0].role_secondary + "\n";
+            try{
+                var data = {name: respChamps.league_champions[0].name};
+                data[args[2]] = args[3];
+                var respUpdate = await api.put("league_champion" , data);
+                if(respUpdate.ok == true){
+                    const ListEmbed = new Discord.RichEmbed()
+                        .setColor("#f92f03")
+                        .setTitle("Here's what changed: ");
+                    var changedInfo = "";
+                    changedInfo += "name: " + respChamps.league_champions[0].name + "\n";
+                    changedInfo += "role_primary: " + respChamps.league_champions[0].role_primary + "\n";
+                    changedInfo += "role_secondary: " + respChamps.league_champions[0].role_secondary + "\n";
+                    changedInfo += "\n\nvvvvv has been changed to vvvvv\n\n";
+                    changedInfo += "name: " + respUpdate.league_champions[0].name + "\n";
+                    changedInfo += "role_primary: " + respUpdate.league_champions[0].role_primary + "\n";
+                    changedInfo += "role_secondary: " + respUpdate.league_champions[0].role_secondary + "\n";
+                }
+            else{
+                message.channel.send("No chamion with that name here!");
             }
-        }else{
-            message.channel.send("No chamion with that name here!");
+        }catch(error2){
+            this.logger.error(error2.response);
         }
     }
 };
