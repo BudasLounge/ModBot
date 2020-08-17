f8745c00a9479f9c31bb7b6718be17c6db09e9e7
JR Buda <jrbuda01@gmail.com>
2020-08-17 14:44:09 -0400

    league commands

--- modules/league/commands/list_champions.js
+++ modules/league/commands/list_champions.js
@@ -36,12 +36,13 @@ module.exports = {
             for(var i = 1; i<respChamps.league_champions.length;i++){
                 output += respChamps.league_champions[i].name + " - " + respChamps.league_champions[i].role_primary + "/" +respChamps.league_champions[i].role_secondary +"\n";
             } 
-            const ListEmbed = new Discord.RichEmbed()
+            message.channel.send(output, {split:true});
+            /*const ListEmbed = new Discord.RichEmbed()
                 .setColor("#f92f03")
                 .setTitle("A list of all champions: ");
                 ListEmbed.addField("A post function update: ", changedInfo);
                 message.channel.send(ListEmbed, {split:true});
-            
+            */
         }
     }
 };
