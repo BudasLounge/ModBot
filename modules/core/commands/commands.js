b02bbb8946a8eeaa11ff49c9ca894786e3d0d6b6
Andrew Graber <recovery22306@gmail.com>
2020-08-17 09:57:14 -0400

    testing

--- modules/core/commands/commands.js
+++ modules/core/commands/commands.js
@@ -99,6 +99,7 @@ module.exports = {
 
                     for(var current_command_name of Array.from(current_module.commands.keys())) {
                         var current_command = current_module.commands.get(current_command_name);
+                        this.logger.info({ command: current_command });
                         if(current_command.syntax.length > longest_syntax.length) {
                             longest_syntax = current_command.syntax;
                         }
