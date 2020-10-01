55c86893c47fe95ced5c95f593bfeb356ac9e291
JR Buda <jrbuda01@gmail.com>
2020-10-01 18:39:46 -0400

    prepping dnd module for new users

--- modules/dnd/commands/lfg_on_off.js
+++ modules/dnd/commands/lfg_on_off.js
@@ -3,9 +3,17 @@ module.exports = {
     description: 'Toggles if you want to be looking for a group or not',
     syntax: 'lfg [on/off]',
     num_args: 0,
-    args_to_lower: false,
+    args_to_lower: true,
     needs_api: true,
     has_state: false,
     async execute(message, args, extra) {
+        let role = message.guild.roles.get("761355940955291678");
+
+        if(args[1] == "on"){
+            message.member.addRole(role);
+        }
+        else if(args[1] == "off"){
+            message.member.removeRole(role);
+        }
     }
 };
