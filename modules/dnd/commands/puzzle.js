06ad03c604d3e960706f013acf39967b513fc0f1
JR Buda <jrbuda01@gmail.com>
2020-10-17 17:57:59 -0400

    puzzles

---
+++ modules/dnd/commands/puzzle.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'puzzle',
+    description: 'Helps you find the answers.',
+    syntax: 'puzzle [info]',
+    num_args: 0,
+    args_to_lower: true,
+    needs_api: true,
+    has_state: false,
+    async execute(message, args, extra) {
+        var api = extra.api;
+        var answersArray = {"first": "Ask Cella for the number", "2319":"It's all in reverse?"};
+        var flag = false;
+        for(var key in answersArray){
+            if(key == message[args1]){
+                flag = true;
+            }
+        }
+        if(flag = true){
+            message.channel.send(answersArray[message[arg1]]);
+        };
+    }
+};
