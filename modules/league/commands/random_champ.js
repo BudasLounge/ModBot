25c6218f3fd30ac5c9547b955440c975b5c8af42
JR Buda <jrbuda01@gmail.com>
2020-10-20 18:17:05 -0400

    updating puzzle info

--- modules/league/commands/random_champ.js
+++ modules/league/commands/random_champ.js
@@ -68,7 +68,7 @@ module.exports = {
                     } catch(error3){
                         this.logger.error(error3.response);
                     }
-                    respChamps = {...respChampsPrim, ...respChampsPrim, ...respChampsSec};
+                    respChamps = {respChampsPrim,respChampsPrim,respChampsSec};
                     this.logger.info(respChamps);
                     var seed = (Math.floor(Math.random() * respChamps.league_champions.length));
                     message.channel.send("<@" + message.member.id + "> "+"Your " + args[1] + " champ is: " + respChamps.league_champions[seed].name);
