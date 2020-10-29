d8a0aa416a86bb5182b83e25260bd9fd7585379d
JR Buda <jrbuda01@gmail.com>
2020-10-29 19:24:02 -0400

    state engine testing

--- modules/league/commands/add_champ.js
+++ modules/league/commands/add_champ.js
@@ -1,22 +1,63 @@
 module.exports = {
-    name: 'state_test',
-    description: 'Testing out state engine',
-    syntax: 'state_test',
+    name: 'new_champ',
+    description: 'To add a new champion to the league database',
+    syntax: 'new_champ',
     num_args: 0,
     args_to_lower: true,
     needs_api: true,
     has_state: true,
     async execute(message, args, extra) {
         var state = extra.state;
+        var api = extra.api;
+
         if(!state.data.has("name")) {
             if(args.length == 2) {
               state.add_data("name", "STRING", args[1]);
-              message.channel.send("Okay, " + state.data.get("name").data + ". I'll remember your name until this state times out!");
+              message.channel.send("Okay, the champion is named: " + state.data.get("name"));
             } else {
-              message.channel.send("Hi there! My name is ModBot! What's yours? (run /state_test <your_name>)");
+              message.channel.send("To add a new champion, start with /new_champ [champion name]");
             }
-          } else {
-            message.channel.send("Hello again, " + state.data.get("name").data);
-          }
+        }else if(!state.data.has("prim_role")){
+            if(args.length == 2) {
+                state.add_data("prim_role", "STRING", args[1]);
+                message.channel.send(state.data.get("name") + " has a primary role of: " + state.data.get("prim_role"));
+              } else {
+                message.channel.send("To add the champion's primary role, enter /new_champ [primary role]");
+              }
+        }else if(!state.data.has("sec_role")){
+            if(args.length == 2) {
+                state.add_data("sec_role", "STRING", args[1]);
+                message.channel.send(state.data.get("name") + " has a secondary role of: " + state.data.get("sec_role"));
+              } else {
+                message.channel.send("To add the champion's secondary role, enter /new_champ [secondary role]");
+              }
+        }else if(!state.data.has("ad_ap")){
+            if(args.length == 2) {
+                if(args[1]=="ad"||args[1]=="ap"){
+                    state.add_data("ad_ap", "STRING", args[1]);
+                    message.channel.send(state.data.get("name") + " is of damage type: " + state.data.get("ad_ap"));
+                } else{
+                    message.channel.send("Please enter '/new_champ ad' or '/new_champ ap' to select a damage type")
+                }
+              } else {
+                message.channel.send("To add the championss damage type, enter /new_champ [damage type (ad or ap only)]");
+              }
+        }
+
+        try{
+            var respNewChamp = await api.post("league_champion", {
+                name:state.data.get("name"),
+                role_primary:state.data.get("prim_role"),
+                role_secondary:state.data.get("sec_role"),
+                ad_ap:state.data.get("ad_ap")
+            });
+        }catch(err){
+            this.logger.error(err);
+        }
+        if(respNewChamp.ok == true){
+            message.channel.send("Successfully added a new champion!");
+        }else{
+            message.channel.send("Hit a snag... try again!");
+        }
     }
 };
