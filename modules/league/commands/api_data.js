089393328e221f80bf9d738fe06ebb37f3a41775
JR Buda <jrbuda01@gmail.com>
2020-11-07 12:47:08 -0500

    adding league commands functionality

---
+++ modules/league/commands/api_data.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'api_test',
+    description: 'For testing league API pulling',
+    syntax: 'api_test [summoner name (case sensitive)',
+    num_args: 0,
+    args_to_lower: true,
+    needs_api: true,
+    has_state: false,
+    async execute(message, args, extra) {
+        let LeagueAPI = require('leagueapiwrapper');
+LeagueAPI = new LeagueAPI("RGAPI-09a3d630-4744-4345-9026-e5368912b158", Region.NA);
+ 
+LeagueAPI.getSummonerByName(args[1])
+    .then(function(accountInfo) {
+        // do something with accountInfo
+    this.logger.info(accountInfo);
+    })
+    .catch(console.log);
+    }
+};
