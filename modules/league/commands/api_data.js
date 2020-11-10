3eed7762ce7532194dfd5a30a360a58936d74644
JR Buda <jrbuda01@gmail.com>
2020-11-10 17:03:28 -0500

    adding league commands functionality

--- modules/league/commands/api_data.js
+++ modules/league/commands/api_data.js
@@ -10,8 +10,7 @@ module.exports = {
         var fs = require('fs');
         logger = this.logger;
         let LeagueAPI = require('leagueapiwrapper');
-        this.logger.info(__dirname + "/../../../riot_token.txt");
-        /*token = fs.readFileSync(__dirname + "/../../../riot_token.txt", "utf8");
+        token = fs.readFileSync("/home/botsriot_token.txt", "utf8");
         LeagueAPI = new LeagueAPI(token, Region.NA);
  
         var matchData = await LeagueAPI.getSummonerByName(args[1])
@@ -28,6 +27,6 @@ module.exports = {
 
     
         var testMatch = await LeagueAPI.getMatch(3593248505);
-        message.channel.send("Here's the match data:" + testMatch);*/
+        message.channel.send("Here's the match data:" + testMatch);
     }
 };
