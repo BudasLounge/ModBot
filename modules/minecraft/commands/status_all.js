1ca0626a0ad39ee8b58050c6a938703c54fdc0e7
JR Buda <jrbuda01@gmail.com>
2021-01-09 12:59:24 -0500

    updating minecraft status commands

--- modules/minecraft/commands/status_all.js
+++ modules/minecraft/commands/status_all.js
@@ -8,8 +8,8 @@ module.exports ={
     has_state: false,
     async execute(message, args, extra) {
         var api = extra.api;
-
         const Discord = require('discord.js');
+        const {getStatus} = require("mc-server-status");
         this.logger.info(">>display_all_servers_status");
         var respServer;
         try{
@@ -19,15 +19,19 @@ module.exports ={
         } catch(error){
             this.logger.error(error.response);
         }
+        if(!respServer.minecraft_servers[0]){
+            return;
+        }
         this.logger.info(respServer.minecraft_servers.length + " servers found...");
         const ListEmbed = new Discord.RichEmbed()
         .setColor("#f92f03")
         .setTitle("List of all minecraft servers: ");
         for(var i = 0;i<respServer.minecraft_servers.length;i++){
-            var nextItem = "";
-            //nextItem += getServerStatus(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip);
-            nextItem += await getServerState(respServer.minecraft_servers[i].short_name, respServer.minecraft_servers[i].port, respServer.minecraft_servers[i].numeric_ip, respServer.minecraft_servers[i].status_api_port);
-            //this.logger.info(nextItem);
+            var nextItem = respServer.minecraft_servers[0].display_name + " is currently online with: " + item.players.online + " players online!\n";
+                nextItem += "Players online:\n";
+                for(var i = 0;i<item.players.online;i++){
+                    nextItem += "- " + item.players.sample[i].name + "\n";
+                }
             ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", nextItem);
         }
         
@@ -35,55 +39,3 @@ module.exports ={
         this.logger.info("<<display_all_servers_status");
     }
 };
-
-/*async function getServerState(server, port, ip){
-    var axios = require('axios');
-    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
-    var response = await axios.get(url);
-    response = response.data;
-    var status = '*'+server+' server is currently offline*'
-    if(response.online) {
-        status = '**'+server+'** server is **online**  -  ';
-        if(response.players.now) {
-            status += '**' + response.players.now + '** people are playing!';
-        } else {
-            status += '*Nobody is playing!*';
-        }
-    }
-    this.logger.info("Returning message: "+status);
-    return status;
-}*/
-async function getServerState(server, port, ip, status_api_port){
-    var axios = require('axios');
-    var url = 'http://mcapi.us/server/status?ip='+ip+'&port=' + port;
-    var response = await axios.get(url);
-    response = response.data;
-    var status = '*'+server+' server is currently offline*';
-    try{
-        if(response.online) {
-            status = '**'+server+'** server is **online**  -  ';
-            if(response.players.now) {
-                status += '**' + response.players.now + '** people are playing!';
-                if(status_api_port != "none"){
-                    status += "\nPlayers: ";
-                    var respPlayers = await axios.get("http://192.168.1.2:" + status_api_port + "/player-list", {});
-                    this.logger.info("returned from api:\n" + respPlayers.data.players);
-                    if(respPlayers.data.players.length == 0) {
-                        status += ".";
-                    } else {
-                        for(var player of respPlayers.data.players) {
-                            status += "\n  - " + player.username;
-                        }
-                    }
-                }
-            } else {
-                status += '*Nobody is playing!*';
-            }
-        }
-    }catch(err){
-        this.logger.error(err);
-    }
-    this.logger.info("Returning message: "+status);
-    return status;
-}
-//http://mcapi.us/server/status?ip=104.218.144.200&port=11160
