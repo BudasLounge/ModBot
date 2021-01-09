6ede49258507aafb704909ffb5b3df2f5ec27be3
JR Buda <jrbuda01@gmail.com>
2021-01-09 13:10:42 -0500

    updating minecraft status commands

--- modules/minecraft/commands/status_all.js
+++ modules/minecraft/commands/status_all.js
@@ -38,8 +38,10 @@ module.exports ={
                 flag = true;
             }
             if(flag == true){
+                this.logger.info("Adding listEmbed for offline server");
                 ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", item);
             }else{
+                this.logger.info("Adding listEmbed for online server");
                 var nextItem = respServer.minecraft_servers[i].display_name + " is currently online with: " + item.players.online + " players online!\n";
                 nextItem += "Players online:\n";
                 for(var i = 0;i<item.players.online;i++){
