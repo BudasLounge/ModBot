d8b94ff914e0af15ca8d855c311934c3599dd57e
JR Buda <jrbuda01@gmail.com>
2021-06-05 06:08:30 -0400

    game lobby tool

---
+++ modules/league/commands/open_game.js
@@ -0,0 +1,39 @@
+module.exports = {
+    name: 'game',
+    description: 'Various ways to interact with games',
+    syntax: 'game [help/open/join/start/end] Also various option flags.',
+    num_args: 0,
+    args_to_lower: true,
+    needs_api: true,
+    has_state: false,
+    async execute(message, args, extra) {
+        var api = extra.api;
+        const Discord = require("discord.js");
+        switch(args[0]){
+            case "open":
+                var respGame;
+                try{
+                    respGame = await api.get("game_joining_master",{
+                        host_id:message.member.id
+                    });
+                } catch(error){
+                    this.logger.error(error.response);
+                }
+                if(respGame[0]){
+                    message.channel.send("You already have a live game! Close it out with /game end");
+                }else{
+                    try{
+                        respGame = await api.post("game_joining_master",{
+                            host_id:message.member.id
+                        });
+                    } catch(error2){
+                        this.logger.error(error2.response);
+                    }
+                    if(respGame.ok){
+                        message.channel.send("Created a game! Others can now join with /game join @host");
+                    }
+                }
+            break;
+                }
+        }
+};
