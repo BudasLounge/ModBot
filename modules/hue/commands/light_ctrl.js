99b6fa8702304aa6e2d2880ac669fa0690b5fd80
JR Buda <jrbuda01@gmail.com>
2021-08-23 11:45:44 -0400

    Hue commands start

--- modules/hue/commands/light_ctrl.js
+++ modules/hue/commands/light_ctrl.js
@@ -9,7 +9,7 @@ module.exports = {
     async execute(message, args, extra) {
       var fs = require('fs');
       var token = fs.readFileSync("../hue_token.txt").toString();
-      const url = `http://192.168.1.58/api/${token}/lights/8/state`;
+      const url = `http://192.168.1.58/api/${token}/lights/7/state`;
     try {
         return await axios.put(url, {
             on: true,
