af09151514b71161c67b12cd0d861db77b1da696
JR Buda <jrbuda01@gmail.com>
2021-08-23 14:45:16 -0400

    Hue commands start

--- modules/hue/commands/light_ctrl.js
+++ modules/hue/commands/light_ctrl.js
@@ -13,29 +13,36 @@ module.exports = {
         var token = await fs.readFileSync("../hue_token.txt").toString();
         token = token.replace(/(\r\n|\n|\r)/gm, "");
         var lightID = args[2];
-
-        var red = args[3];
-        var green = args[4];
-        var blue = args[5];
-        red = (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
-        green = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
-        blue = (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
-        var X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
-        var Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
-        var Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
-        var fx = X / (X + Y + Z);
-        var fy = Y / (X + Y + Z);
+        if(lightID === "6"){
+            var red = args[3];
+            var green = args[4];
+            var blue = args[5];
+            red = (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
+            green = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
+            blue = (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
+            var X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
+            var Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
+            var Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
+            var fx = X / (X + Y + Z);
+            var fy = Y / (X + Y + Z);
+        }
 
         var url = `http://192.168.1.58/api/${token}/lights/${lightID}/state`;
         var lightResp;
 
       if(args[1] == "on"){
         try {
-            lightResp = await axios.put(url, {
-                on: true,
-                xy:[fx,fy],
-                //hue: parseInt(args[3]),
-            });
+            if(lightID === "6"){
+                lightResp = await axios.put(url, {
+                    on: true,
+                    xy:[fx,fy],
+                });
+            }
+            else{
+                lightResp = await axios.put(url, {
+                    on: true,
+                });
+            }
         } catch (err) {
             this.logger.error(err);
         }
