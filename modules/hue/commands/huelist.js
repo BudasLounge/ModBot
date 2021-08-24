f5439b4bc645b5393708acc89c2c1cd522321c00
JR Buda <jrbuda01@gmail.com>
2021-08-24 12:13:38 -0400

    huelist

--- modules/hue/commands/huelist.js
+++ modules/hue/commands/huelist.js
@@ -18,6 +18,6 @@ module.exports = {
         });
         var stringedResp = Object.values(lightResp.data);
         this.logger.info(stringedResp);
-        message.channel.send(jJSON.stringify(stringedResp));
+        message.channel.send(JSON.stringify(stringedResp));
     }
 }
