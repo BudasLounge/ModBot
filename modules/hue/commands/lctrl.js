2008e8f97f395c688ab1d525ab6145738d213cb9
JR Buda <jrbuda01@gmail.com>
2021-08-27 15:04:12 -0400

    new server for testing

--- modules/hue/commands/lctrl.js
+++ modules/hue/commands/lctrl.js
@@ -57,7 +57,7 @@ module.exports = {
                 args[5] = Math.random() * (255-1) +1;
             }
         if((Number.isInteger(parseInt(args[3])) && Number.isInteger(parseInt(args[4])) && Number.isInteger(parseInt(args[5]))) || args[1] == "off"){
-            //message.channel.send("Valid RGB!");
+            
         }else{
             message.channel.send("Invalid RGB, defaulting to 100 100 100");
             args[3] = 100;
