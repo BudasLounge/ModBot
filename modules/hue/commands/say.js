956a7bbe05114bdd84035e7eb879b74e2649b463
JR Buda <jrbuda01@gmail.com>
2021-08-30 18:23:23 -0400

    tts

--- modules/hue/commands/say.js
+++ modules/hue/commands/say.js
@@ -7,6 +7,30 @@ module.exports = {
     needs_api: false,
     has_state: false,
     async execute(message, args, extra) {
+
+        const moment = require('moment');
+        var d = new Date(); // current time
+        var hours = d.getHours();
+        var now = moment();
+        
+
+        if((hours >= 8 && (hours < 23)) || message.author.id === "185223223892377611"){
+
+        }else{
+            var deadline = now.clone().hour(8).minute(0).second(0);
+            var opening_time;
+            if(now.isAfter(deadline)) {
+                var tomorrow  = moment(new Date()).add(1,'days').hour(8).minute(0).second(0);
+                opening_time = tomorrow.diff(now, "hours") + ' hrs, ' + (tomorrow.diff(now, "minutes") % 60) + ' mins'
+                //opening_time = tomorrow.from(now);
+            }else {
+                opening_time = deadline.diff(now, "hours") + ' hrs, ' + (deadline.diff(now, "minutes") % 60) + ' mins'
+                //opening_time = deadline.from(now);
+            }
+            message.channel.send("This command is closed. It will open again " + opening_time + ". Try again later!");
+            return;
+        }
+        
         const Filter = require('bad-words');
         filter = new Filter();
         filter.removeWords()
