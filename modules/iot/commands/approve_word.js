aa0a1cf25b4bc360c31a06b1bb85261aee1a8d1d
JR Buda <jrbuda01@gmail.com>
2021-09-03 10:27:23 -0400

    tts

--- modules/iot/commands/approve_word.js
+++ modules/iot/commands/approve_word.js
@@ -8,14 +8,14 @@ module.exports = {
     has_state: false,
     async execute(message, args, extra) {
         var api = extra.api;
+        try{
+            var respCheckWord = await api.get("allowed_word",{
+                word:args[1]
+            })
+        }catch(err2){
+            this.logger.error(err2);
+        }
         if(message.author.id === "185223223892377611"){
-            try{
-                var respCheckWord = await api.get("allowed_word",{
-                    word:args[1]
-                })
-            }catch(err2){
-                this.logger.error(err2);
-            }
             if(!respCheckWord.allowed_words[0]){
                 try{
                     var respApprovedWords = await api.post("allowed_word",{
@@ -50,19 +50,23 @@ module.exports = {
                 }
             }
         }else{
-            try{
-                var respApprovedWords = await api.post("allowed_word",{
-                    word:args[1],
-                    approved:0
-                })
-            }catch(err){
-                this.logger.error(err);
-            }
-            if(respApprovedWords.ok){
-                message.channel.send("Word was sent for approval!");
-            }
-            else{
-                message.channel.send("Failed to send for approval, try again!");
+            if(respCheckWord[0]){
+                message.channel.send("That word is already being queued for approval.");
+            }else{
+                try{
+                    var respApprovedWords = await api.post("allowed_word",{
+                        word:args[1],
+                        approved:0
+                    })
+                }catch(err){
+                    this.logger.error(err);
+                }
+                if(respApprovedWords.ok){
+                    message.channel.send("Word was sent for approval!");
+                }
+                else{
+                    message.channel.send("Failed to send for approval, try again!");
+                }
             }
         }
     }
