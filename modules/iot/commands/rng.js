b73bb76b9f3868cf8aca11b76bbff64c96eb2dd7
JR Buda <jrbuda01@gmail.com>
2021-09-06 19:08:40 -0400

    rng command

---
+++ modules/iot/commands/rng.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'rng',
+    description: 'Random Number Generator',
+    syntax: 'rng [optional starting number] [optional ending number]',
+    num_args: 0,
+    args_to_lower: false,
+    needs_api: false,
+    has_state: false,
+    async execute(message, args, extra) {
+        var starting = 1;
+        var ending = 10;
+        if(Number.isInteger(parseInt(args[1]))){
+            starting = args[1];
+        }
+        if(Number.isInteger(parseInt(args[2]))){
+            ending = args[2];
+        }
+        Math.floor(Math.random() * (ending - starting) + starting);
+    }
+}
