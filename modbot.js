70983612d5c739f16e36d97dfe2dc8601a5e4b72
JR Buda <jrbuda01@gmail.com>
2021-09-07 11:43:01 -0400

    discordjs update

--- modbot.js
+++ modbot.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 var request = require('request');
 
 var {Client, Intents} = require('discord.js');
-var client = new Client({ ws: {intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }});
+var client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 var config = JSON.parse(fs.readFileSync('modbot.json'));
 
