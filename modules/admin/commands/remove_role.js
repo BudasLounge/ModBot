6987ff10e92f373dd6a273db1ff1f2623959c67b
JR Buda <jrbuda01@gmail.com>
2021-09-08 15:47:02 -0400

    remove_role command

---
+++ modules/admin/commands/remove_role.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: 'remove_role',
+    description: 'Removes a specified role from a specified user',
+    syntax: 'remove_role [@user] [role]',
+    num_args: 2,
+    args_to_lower: true,
+    needs_api: false,
+    has_state: false,
+    async execute(message, args, extra) {
+        const Discord = require('discord.js');
+        var strLength = 0;
+		var messageString = "";
+		var role = "";
+		var counter = 0;
+        var member = message.mentions.members.first();
+		for(let i=2;i<args.length;i++){
+			//Manages current string length of arguments combined
+			strLength += args[i].length;
+			messageString += args[i];
+			//Check to see if a role exists using that begins with the collective messageString
+			role = message.guild.roles.cache.find(role => role.name.toLowerCase().includes(messageString));
+			if(!role){
+				//If role not found then return to string from previous iteration
+				messageString = messageString.substring(0, strLength - args[i].length - 1);
+				break;
+			}
+			//Add space
+			messageString += " ";
+			strLength ++;
+		counter++;
+		}
+        try{
+            role = message.guild.roles.cache.find(role => role.name.toLowerCase() === messageString.trim());
+            member.roles.remove(role.id);
+        }
+        catch(err){
+            this.logger.error(err.message);
+            message.channel.send("Role removing failed!");
+            return;
+        }
+        const ListEmbed = new Discord.MessageEmbed()
+        .setTitle(`Made this edit to ${member.user.username}:`)
+        .setDescription("Removed role: "+role.name);
+        message.channel.send(ListEmbed);
+    }
+}
