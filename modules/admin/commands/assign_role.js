e682a73773e501877495e949cd74bdac7abee0ff
JR Buda <jrbuda01@gmail.com>
2021-09-08 13:44:42 -0400

    assign roles command

--- modules/admin/commands/assign_role.js
+++ modules/admin/commands/assign_role.js
@@ -7,6 +7,28 @@ module.exports = {
     needs_api: false,
     has_state: false,
     async execute(message, args, extra) {
-
+        var strLength = 0;
+		var messageString = "";
+		var role = "";
+		var counter = 0;
+        var member = message.mentions.users.first();
+		for(let i=1;i<args.length;i++){
+			//Manages current string length of arguments combined
+			strLength += args[i].length;
+			messageString += args[i];
+			//Check to see if a role exists using that begins with the collective messageString
+			role = message.guild.roles.cache.find(role => role.name.toLowerCase().includes(messageString));
+			if(!role){
+				//If role not found then return to string from previous iteration
+				messageString = messageString.substring(0, strLength - args[i].length - 1);
+				break;
+			}
+			//Add space
+			messageString += " ";
+			strLength ++;
+		counter++;
+		}
+        role = message.guild.roles.cache.find(role => role.name.toLowerCase() === messageString.trim());
+        member.roles.add(role.id);
     }
 }
