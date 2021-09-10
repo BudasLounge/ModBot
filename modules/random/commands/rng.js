3c59f97157728b91c0ecba1a9537ff50eff4ab39
JR Buda <jrbuda01@gmail.com>
2021-09-10 10:38:52 -0400

    added random commands folder

--- modules/random/commands/rng.js
+++ modules/random/commands/rng.js
@@ -13,7 +13,7 @@ module.exports = {
             if(args[1]>0){
                 starting = args[1];
                 if(Number.isInteger(parseInt(args[2]))){
-                    if(args[2]>args[1]){
+                    if(args[2]>=args[1]){
                         ending = args[2];
                     }else{
                         starting = 1;
