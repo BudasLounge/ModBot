15c7427afdb287fc9d9ede463bbfec6523dd2511
JR Buda <jrbuda01@gmail.com>
2021-09-13 10:57:48 -0400

    v13 update

--- modbot.js
+++ modbot.js
@@ -5,7 +5,7 @@ var shell = require('shelljs');
 
 //var discord = require('discord.js');
 const {Intents, Client} = require('discord.js');
-const client = new Client({ intents: Intents.FLAGS.GUILD_MEMBERS });
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
 
 var config = JSON.parse(fs.readFileSync('modbot.json'));
 
