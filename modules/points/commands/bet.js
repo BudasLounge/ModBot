2a4218fafef627f09707a2ff7616a831a3370ff6
JR Buda <jrbuda01@gmail.com>
2021-09-12 23:12:57 -0400

    added points folder

---
+++ modules/points/commands/bet.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'bet',
+    description: 'Opens up a bet.',
+    syntax: 'bet [any] [additional] [arguments]',
+    num_args: 0,//minimum amount of arguments to accept
+    args_to_lower: true,//if the arguments should be lower case
+    needs_api: true,//if this command needs access to the api
+    has_state: false,//if this command uses the state engine
+    async execute(message, args, extra) {
+        var api = extra.api;
+        const MessageButton, MessageActionRow = require('discord.js');
+
+        const row = new MessageActionRow()
+        .addComponents(
+        new MessageButton()
+            .setCustomId('primary')
+            .setLabel('Primary')
+            .setStyle('PRIMARY'),
+        );
+
+        message.channel.send({components: [row]});
+    }
+}
---
+++ modules/points/events.js
@@ -0,0 +1,13 @@
+var ApiClient = require("../../core/js/APIClient.js");
+var api = new ApiClient();
+
+async function onButtonClick(interaction){
+	this.logger.info(interaction);
+    
+}
+
+function register_handlers(event_registry) {
+    event_registry.register('interactionCreate', onButtonClick);
+}
+
+module.exports = register_handlers;
