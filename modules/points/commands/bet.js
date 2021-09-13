43c7034df3c8bf979d266b6aec427cb858983d03
JR Buda <jrbuda01@gmail.com>
2021-09-12 23:13:51 -0400

    added points folder

--- modules/points/commands/bet.js
+++ modules/points/commands/bet.js
@@ -8,7 +8,7 @@ module.exports = {
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
         var api = extra.api;
-        const MessageButton, MessageActionRow = require('discord.js');
+        const {MessageButton, MessageActionRow}= require('discord.js');
 
         const row = new MessageActionRow()
         .addComponents(
