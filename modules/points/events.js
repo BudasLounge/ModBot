77377ae567fad190b7759183ad6daefab4d27168
JR Buda <jrbuda01@gmail.com>
2021-09-13 13:20:57 -0400

    v13 update

--- modules/points/events.js
+++ modules/points/events.js
@@ -4,11 +4,10 @@ var api = new ApiClient();
 async function onButtonClick(interaction){
     if(!interaction.isButton()) return;
 	this.logger.info(interaction);
-    
 }
 
 function register_handlers(event_registry) {
-    event_registry.register('clickButton', onButtonClick());
+    event_registry.register('clickButton', onButtonClick);
 }
 
 module.exports = register_handlers;
