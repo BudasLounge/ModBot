40b273448ffaba5671287238a7c3fef5bd463831
JR Buda <jrbuda01@gmail.com>
2021-09-13 14:50:21 -0400

    Betting buttons

--- modules/points/commands/bet.js
+++ modules/points/commands/bet.js
@@ -8,7 +8,38 @@ module.exports = {
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
         var api = extra.api;
+        var init_id = message.member.user.id;
+        var respCheckBal;
+        try{
+            respCheckBal = await api.get("bet_point",{
+                discord_user_id:init_id,
+                discord_server_id:message.guild.id
+            })
+        }catch(err){
+            this.logger.error(err.message);
+        }
+        if(respCheckBal.bet_points[0]){
+            if(!respCheckBal.bet_points[0]>0){
+                message.channel.send({content : "You don't have enough to do that bet!"});
+                return;
+            }
+        }else{
+            message.channel.send({content: "You need to run /point_start in order to get in the system!"});
+            return;
+        }
+
+
+
+
         var serial = makeid(10);
+        var init_name = message.member.user.username;
+        var bet_amount;
+        if(Number.isInteger(parseInt(args[1].slice(0,-1)))){
+            
+        }
+        if(Number.isInteger(parseInt(args[1])))
+
+
         const {MessageButton,MessageActionRow} = require('discord.js');
         const row = new MessageActionRow()
 			.addComponents(
