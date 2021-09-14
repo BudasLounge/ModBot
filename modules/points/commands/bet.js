e2aa36249fc945cbaa5b1e56c4c820bd82fb9923
JR Buda <jrbuda01@gmail.com>
2021-09-14 10:11:32 -0400

    Betting buttons

--- modules/points/commands/bet.js
+++ modules/points/commands/bet.js
@@ -103,7 +103,7 @@ module.exports = {
                 const outputEmbed = new MessageEmbed()
                 .setTitle("New bet created")
                 .addField(init_name.toString() + " has placed a bet for: " + bet_amount.toString() + " " + respCheckServer.bet_configs[0].point_name + "s", "Use the buttons below to partake in the bet!");
-                await message.reply({content: "New bet", embeds: [outputEmbed], components: [BetWin, ForBet, AgainstBet]});
+                await message.reply({/*content: "New bet",*/ embeds: [outputEmbed], components: [BetWin, ForBet, AgainstBet]});
             }
         }
 }
