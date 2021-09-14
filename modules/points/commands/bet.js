887bcc587d0fb9943e8b457208b91d8b9c1cac82
JR Buda <jrbuda01@gmail.com>
2021-09-14 00:46:54 -0400

    Betting buttons

--- modules/points/commands/bet.js
+++ modules/points/commands/bet.js
@@ -8,86 +8,98 @@ module.exports = {
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
         var api = extra.api;
+        const {MessageEmbed} = require('discord.js');
         var init_id = message.member.user.id;
-        var respCheckBal;
+        var respCheckServer;
         try{
-            respCheckBal = await api.get("bet_point",{
-                discord_user_id:init_id,
-                discord_server_id:message.guild.id
+            respCheckServer = await api.get("bet_config",{
+                discord_server_id:server_id
             })
         }catch(err){
             this.logger.error(err.message);
         }
-        if(respCheckBal.bet_points[0]){
-            if(!respCheckBal.bet_points[0]>0){
-                message.channel.send({content : "You don't have enough to do that bet!"});
+        if(respCheckServer.bet_configs[0]){
+            var respCheckBal;
+            try{
+                respCheckBal = await api.get("bet_point",{
+                    discord_user_id:init_id,
+                    discord_server_id:message.guild.id
+                })
+            }catch(err){
+                this.logger.error(err.message);
+            }
+            if(respCheckBal.bet_points[0]){
+                if(!respCheckBal.bet_points[0].points_total>0){
+                    message.channel.send({content : "You don't have enough to do that bet!"});
+                    return;
+                }
+            }else{
+                message.channel.send({content: "You need to run /point_start in order to get in the system!"});
                 return;
             }
-        }else{
-            message.channel.send({content: "You need to run /point_start in order to get in the system!"});
-            return;
-        }
-
-
-
-
-        var serial = makeid(10);
-        var init_name = message.member.user.username;
-        var bet_amount;
-        if(args[1]){
-            if(Number.isInteger(parseInt(args[1].slice(0,-1)))){
-                
+            var serial = makeid(10);
+            var init_name = message.member.user.username;
+            var bet_amount;
+            if(args[1]){
+                if(Number.isInteger(parseInt(args[1].slice(0,-1))) && args[1].slice(0,-1)==="%"){
+                    bet_amount = parseInt(respCheckBal.bet_points[0].points_total) * args[1];
+                }
+                if(Number.isInteger(parseInt(args[1]))){
+                    bet_amount = parseInt(args[1]);
+                }
             }
-            if(Number.isInteger(parseInt(args[1]))){
 
-            }
-        }
-
-        const {MessageButton,MessageActionRow} = require('discord.js');
-        const ForBet = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId(serial+"-forhalf")
-                    .setLabel('Bet Half')
-                    .setStyle('SUCCESS'),
-                new MessageButton()
-                    .setCustomId(serial+"-forequal")
-                    .setLabel('Bet Equal')
-                    .setStyle('SUCCESS'),
-                new MessageButton()
-                    .setCustomId(serial+"-fordouble")
-                    .setLabel('Bet Double')
-                    .setStyle('SUCCESS'),
-            );
-            const AgainstBet = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId(serial+"-againsthalf")
-                    .setLabel('Bet Half')
-                    .setStyle('DANGER'),
-                new MessageButton()
-                    .setCustomId(serial+"-againstequal")
-                    .setLabel('Bet Equal')
-                    .setStyle('DANGER'),
-                new MessageButton()
-                    .setCustomId(serial+"-againstdouble")
-                    .setLabel('Bet Double')
-                    .setStyle('DANGER'),
-            );
-            const BetWin = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId(serial+"w")
-                    .setLabel('Creator Won')
-                    .setStyle('SUCCESS'),
+            const {MessageButton,MessageActionRow} = require('discord.js');
+            const ForBet = new MessageActionRow()
+                .addComponents(
                     new MessageButton()
-                    .setCustomId(serial+"l")
-                    .setLabel('Creator Lost')
-                    .setStyle('DANGER'),
-            );
-
-        await message.reply({content: "Returned!", components: [BetWin, ForBet, AgainstBet]});
-    }
+                        .setCustomId(serial+"-forhalf")
+                        .setLabel('Bet Half')
+                        .setStyle('SUCCESS'),
+                    new MessageButton()
+                        .setCustomId(serial+"-forequal")
+                        .setLabel('Bet Equal')
+                        .setStyle('SUCCESS'),
+                    new MessageButton()
+                        .setCustomId(serial+"-fordouble")
+                        .setLabel('Bet Double')
+                        .setStyle('SUCCESS'),
+                );
+                const AgainstBet = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId(serial+"-againsthalf")
+                        .setLabel('Bet Half')
+                        .setStyle('DANGER'),
+                    new MessageButton()
+                        .setCustomId(serial+"-againstequal")
+                        .setLabel('Bet Equal')
+                        .setStyle('DANGER'),
+                    new MessageButton()
+                        .setCustomId(serial+"-againstdouble")
+                        .setLabel('Bet Double')
+                        .setStyle('DANGER'),
+                );
+                const BetWin = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId(serial+"w")
+                        .setLabel('Creator Won')
+                        .setStyle('SUCCESS'),
+                        new MessageButton()
+                        .setCustomId(serial+"l")
+                        .setLabel('Creator Lost')
+                        .setStyle('DANGER'),
+                );
+                if(respCheckServer.bet_configs[0].point_name.charAt(respCheckServer.bet_configs[0].point_name.length-1) === "s"){
+                    respCheckServer.bet_configs[0].point_name = respCheckServer.bet_configs[0].point_name.substring(0, respCheckServer.bet_configs[0].point_name.length-1);
+                }
+                const outputEmbed = new MessageEmbed()
+                .setTitle("New bet created")
+                .addField(message.member.user.username.toString() + " has placed a bet for: " + bet_amount.toString() + " " + respCheckServer.bet_configs[0].point_name + "s", "Use the buttons below to partake in the bet!");
+                await message.reply({content: "New bet", embeds: [outputEmbed], components: [BetWin, ForBet, AgainstBet]});
+            }
+        }
 }
 
 function makeid(length) {
