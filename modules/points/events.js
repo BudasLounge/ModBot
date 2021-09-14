76d22caa8edd4c6ed92d6809823e7b266ebeb07c
JR Buda <jrbuda01@gmail.com>
2021-09-14 12:16:42 -0400

    Betting buttons

--- modules/points/events.js
+++ modules/points/events.js
@@ -3,8 +3,7 @@ var api = new ApiClient();
 
 async function onButtonClick(button){
     if (!button.isButton()) return;
-	button.channel.send({content: "You clicked it!"});
-    button.channel.send({content: button.customId});
+	this.logger.info(button);
     button.deferUpdate();
 }
 
