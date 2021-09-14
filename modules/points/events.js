6900a3c888fc7d27f0b0744e3ca2766441a64332
JR Buda <jrbuda01@gmail.com>
2021-09-14 12:20:08 -0400

    Betting buttons

--- modules/points/events.js
+++ modules/points/events.js
@@ -1,5 +1,6 @@
 var ApiClient = require("../../core/js/APIClient.js");
 var api = new ApiClient();
+var LogHandler = require('../core/js/log_handler.js');
 var logger = LogHandler.build_logger("../" + __dirname + "/" + config.log_folder);
 
 async function onButtonClick(button){
