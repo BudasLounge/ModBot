d772d7afa02ca4e50238e9c133490531dce99099
JR Buda <jrbuda01@gmail.com>
2021-09-15 00:39:39 -0400

    Betting buttons

--- modules/points/events.js
+++ modules/points/events.js
@@ -7,11 +7,16 @@ async function onButtonClick(button){
     var respCheckMaster;
     try{
         respCheckMaster = await api.get("bet_master",{
-
+            serial:serial
         })
     }catch(err){
         this.logger.error(err.message);
     }
+    if(!respCheckMaster.bet_masters[0]){
+        button.channel.send({content: "Couldn't find that bet, as an admin for help"});
+        return;
+    }
+    
     console.log(button.customId);
     button.channel.send({content: "Serial: " + serial});
     button.deferUpdate();
