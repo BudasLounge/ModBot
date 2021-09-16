6db26c23957104b6868d4917a57d44ccea5b82b8
JR Buda <jrbuda01@gmail.com>
2021-09-16 12:40:52 -0400

    Betting buttons

--- modules/points/events.js
+++ modules/points/events.js
@@ -115,7 +115,7 @@ async function onButtonClick(button){
                     }catch(err){
                         console.log(err);
                     }
-                    output += againstBet[j].better_discord_username + " added " + parseInt(pot/forBet.length) + " to their wealth\n";
+                    output += againstBet[k].better_discord_username + " added " + parseInt(pot/forBet.length) + " to their wealth\n";
                 }
             }
             const listWinners = new MessageEmbed()
