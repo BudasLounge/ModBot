98e4cc7b0db93592cdee5be6c858730232d9c3c4
JR Buda <jrbuda01@gmail.com>
2021-09-16 12:29:26 -0400

    Betting buttons

--- modules/points/commands/point_start.js
+++ modules/points/commands/point_start.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Grants access to the point system. User ID is collected',
     syntax: 'point_start',
     num_args: 0,
-    args_to_lower: false,
+    args_to_lower: true,
     needs_api: true,
     has_state: false,
     async execute(message, args, extra) {
