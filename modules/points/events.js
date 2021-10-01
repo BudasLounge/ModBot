7fe396cdc0f1fa0d40ea471cb7c0271b9e8bbf59
JR Buda <jrbuda01@gmail.com>
2021-10-01 00:58:21 -0400

    fixing bet payout text

--- modules/points/events.js
+++ modules/points/events.js
@@ -179,6 +179,7 @@ if(button.isButton()){
                     output += againstBet[k].better_discord_username + " added " + winnings.toString() + " to their wealth\n";
                 }
             }
+            button.channel.send({content: "Bet with total pot of: " + pot + ". Which had " + forBet.length + " for it and " + againstBet.length + " against it. The winners will split: " + winTotal.toString()});
             const listWinners = new MessageEmbed()
             .setColor("#f92f03")
             .setTitle("Here are the winners of this bet: ")
