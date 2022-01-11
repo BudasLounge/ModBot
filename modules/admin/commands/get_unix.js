21dfbfb2561d61aa643945393855a46c30021973
JR Buda <jrbuda01@gmail.com>
2022-01-11 17:13:01 -0500

    command for getting time in unix

--- modules/admin/commands/get_unix.js
+++ modules/admin/commands/get_unix.js
@@ -7,7 +7,7 @@ module.exports = {
     needs_api: true,//if this command needs access to the api
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
-        if(args[1]==""){
+        if(!args[1]){
             message.channel.send({ content: "Please enter a datetime stamp for this command!"});
             return
         }
