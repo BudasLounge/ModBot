14bb441d1a21587f8f5c8c33cb4567a6b59cafb4
JR Buda <jrbuda01@gmail.com>
2022-01-11 17:16:37 -0500

    command for getting time in unix

--- modules/admin/commands/get_unix.js
+++ modules/admin/commands/get_unix.js
@@ -1,17 +1,18 @@
 module.exports = {
     name: 'get_unix',
-    description: 'Returns given time in unix time. Time should be in YYYY-MM-DD HH:MM:SS time stamp, surrounded entirely in quotes',
+    description: 'Returns given time in unix time. Time should be in YYYY-MM-DD HH:MM:SS time stamp',
     syntax: 'get_unix ["date time string"]',
     num_args: 0,//minimum amount of arguments to accept
     args_to_lower: false,//if the arguments should be lower case
     needs_api: true,//if this command needs access to the api
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
-        if(!args[1]){
-            message.channel.send({ content: "Please enter a datetime stamp for this command!\nYYYY-MM-DD HH:MM:SS time stamp, surrounded entirely in quotes"});
+        if(!args[1] || !args[2]){
+            message.channel.send({ content: "Please enter a datetime stamp for this command!\nYYYY-MM-DD HH:MM:SS time stamp"});
             return
         }
-        var unixTimeStamp = Math.floor(new Date(args[1]).getTime()/1000);
+        var dateTime = args[1] + " " + args[2];
+        var unixTimeStamp = Math.floor(new Date(dateTime).getTime()/1000);
         message.channel.send({ content: unixTimeStamp.toString() });
     }
 }
