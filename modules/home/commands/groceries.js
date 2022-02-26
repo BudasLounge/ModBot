d886b13f3fe32f7811c727c676b7cec35659c0d8
JR Buda <jrbuda01@gmail.com>
2022-02-26 17:26:35 -0500

    groceries

--- modules/home/commands/groceries.js
+++ modules/home/commands/groceries.js
@@ -1,12 +1,74 @@
 module.exports = {
-    name: '[what word you type to activate this command]',
-    description: '[what the command should do]',
-    syntax: '[activation word] [any] [additional] [arguments]',
+    name: 'groceries',
+    description: 'Brings up the grocery list. No argument will return the current list.',
+    syntax: 'groceries [add/drop/clear] [name of item]',
     num_args: 0,//minimum amount of arguments to accept
     args_to_lower: false,//if the arguments should be lower case
     needs_api: true,//if this command needs access to the api
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
-
+        const fs = require('fs');
+        const groceryList = 'groceryList.txt';
+        fs.access(groceryList, fs.F_OK, (err) => {
+            if (err) {
+              this.logger.error("Could not find the groceryList, creating file");
+              fs.writeFile('groceryList.txt', function(err){
+                  if(err) this.logger.error(err.message);
+                  this.logger.info("File created");
+              });
+            }
+        })
+        var groceries = [];
+        fs.readFile('groceryList.txt', function(err, data) {
+            if(err) throw err;
+            groceries = data.toString().split("\n");
+        });
+        if(args[1] == "add"){
+            if(args[2]){
+                args.shift();
+                args.shift();
+                var item = args.join(" ");
+                fs.appendFile('groceryList' , item, function (err) {
+                    if (err){
+                        this.logger.error(err.message);
+                        message.channel.send("An error occurred. Item not added.");
+                        return;
+                    }
+                    this.logger.info("Added " + item + " to the grocery list");
+                    message.channel.send({content: "Added " + item + " to the grocery list"})
+                });
+            }else{
+                message.channel.send({content: "You did not put a name to add"});
+            }
+        }else if(args[1] == "drop"){
+            args.shift();
+            args.shift();
+            var item = args.join(" ");
+            groceries.splice(groceries.indexOf(item),1);
+            var txtGroceries = "";
+            for(var i = 0;i<groceries.length;i++){
+                txtGroceries+=groceries[i]+"\n"
+            }
+            fs.writeFile('groceryList', txtGroceries, function(err){
+                if(err){
+                    this.logger.error(err.message);
+                    message.channel.send({content: "An error occurred. Item not dropped."});
+                    return;
+                }
+            })
+        }else if(args[1] == "clear"){
+            fs.writeFile('groceryList', function(err){
+                if(err){
+                    this.logger.error(err.message);
+                    message.channel.send({content: "An error occurred. List not cleared."});
+                    return;
+                }
+            })
+        }
+        var output = "";
+        for(var i = 0;i<groceries.length;i++){
+            output+=groceries[i]+"\n"
+        }
+        message.channel.send({content: "Here is the current list of groceries: " + output})
     }
 }
