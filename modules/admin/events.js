514f5d6a4e59d0d7a18979831c72b23d19fed00a
JR Buda <jrbuda01@gmail.com>
2022-04-11 11:13:14 -0400

    adding voice time tracking

--- modules/admin/events.js
+++ modules/admin/events.js
@@ -25,7 +25,7 @@ async function userJoinsVoice(oldMember, newMember){
     let newUserChannel = newMember.channelId;
     let oldUserChannel = oldMember.channelId;
 
-    console.log(newMember.user.username);
+    //console.log(newMember.user.username);
     //console.log(oldMember);
     let user = newMember.guild.members.cache.get(newMember.id);
     if(newUserChannel != undefined){
