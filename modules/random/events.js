49b9c1118c23d72495445a56d878375d4d9f54ee
JR Buda <jrbuda01@gmail.com>
2022-05-09 13:41:16 -0400

    voicetime channel tracking update

--- modules/random/events.js
+++ modules/random/events.js
@@ -567,7 +567,7 @@ async function onButtonClick(button){
             }
             var flag = false;
             for(var j = 0;j<totalTime.length;j++){
-                if(totalTime[j][0] == respVoice.voice_trackings[i].channelNameUse){
+                if(totalTime[j][0] == channelNameUse){
                     logger.info("Adding to existing row.")
                     totalTime[j][1] += Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))
                     flag = true;
@@ -576,7 +576,7 @@ async function onButtonClick(button){
             }
             if(!flag){
                 logger.info("Creating a new row.")
-                totalTime.push([respVoice.voice_trackings[i].channelNameUse, Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))])
+                totalTime.push([channelNameUse, Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))])
             }
         }
         logger.info("Printing array to a table, will only show up in live console, not logs...")
