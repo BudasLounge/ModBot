4ff466add2bf8572027bc214f3a5042445220269
JR Buda <jrbuda01@gmail.com>
2022-05-26 18:59:45 -0400

    adding 30 and 7 day tops

--- modules/random/events.js
+++ modules/random/events.js
@@ -569,6 +569,7 @@ async function onButtonClick(button){
             }
             var flag = false;
             for(var j = 0;j<totalTime.length;j++){
+                logger.info(totalTime[j][0])
                 if(totalTime[j][0] == channelNameUse.name){
                     logger.info("Adding to existing row.")
                     totalTime[j][1] += Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))
