251941740d933b8171a0a37457cdf247db3fb84b
JR Buda <jrbuda01@gmail.com>
2022-06-08 16:02:41 -0400

    adding speak command

--- modules/random/commands/speak.js
+++ modules/random/commands/speak.js
@@ -9,7 +9,7 @@ module.exports = {
     async execute(message, args, extra) {
 
         const discordTTS=require("discord-tts");
-        const {AudioPlayer, createAudioResource, StreamType, entersState, VoiceConnectionStatus, joinVoiceChannel} = require('discord.js');
+        const {AudioPlayer, createAudioResource, StreamType, entersState, VoiceConnectionStatus, joinVoiceChannel} = require('discord.js/voice');
         let voiceConnection;
         let audioPlayer=new AudioPlayer();
 
