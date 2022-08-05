227d3f702bec9e33922b3cacec6108806399f3cb
JR Buda <jrbuda01@gmail.com>
2022-08-05 11:10:44 -0700

    making new invite command

--- modules/dnd/events.js
+++ modules/dnd/events.js
@@ -18,7 +18,7 @@ async function onButtonClick(button){
                 logger.error(error2.message)
             }
             if(!respCampaign.dnd_campaigns[0]){
-                message.channel.send({content: "I can't find a campaign linked for you. Ask an admin to help you get started!"});
+                button.channel.send({content: "I can't find a campaign linked for you. Ask an admin to help you get started!"});
                 return;
             }
 
