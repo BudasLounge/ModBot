2bb9b32dc6e4b9cbaf1d982b402450fd4716045d
JR Buda <jrbuda01@gmail.com>
2022-08-05 10:14:40 -0700

    making new invite command

--- modules/dnd/events.js
+++ modules/dnd/events.js
@@ -9,7 +9,7 @@ async function onButtonClick(button){
             await button.reply({content: "This invite was not made for you.", ephemeral: true})
         }
         var IDcheck = button.customId.slice("_");
-        logger.info(IDcheck.substring(0))
+        logger.info(IDcheck)
         if(IDcheck.includes("A")){
             try{
                 var respAddToCampaign = api.post("dnd_players_in_campaign",{
