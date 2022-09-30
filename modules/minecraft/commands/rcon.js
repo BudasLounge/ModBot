b35c4d464debcc8a68b47b35ddc169d2b3d195bc
JR Buda <jrbuda01@gmail.com>
2022-09-30 09:00:10 -0700

    creating rcon for minecraft

--- modules/minecraft/commands/rcon.js
+++ modules/minecraft/commands/rcon.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'rcon',
     description: 'pushes an rcon command to a minecraft server',
     syntax: 'rcon [minecraft_shortname] [rcon_command/minecraft_server_command]',
-    num_args: 2,//minimum amount of arguments to accept
+    num_args: 1,//minimum amount of arguments to accept
     args_to_lower: true,//if the arguments should be lower case
     needs_api: true,//if this command needs access to the api
     has_state: false,//if this command uses the state engine
