92a8257c6cf17e99de240f3d33644d7d1e5c3a9e
JR <jrbuda01@gmail.com>
2022-11-11 13:53:16 -0800

    modal campaign creator!

--- modules/dnd/events.js
+++ modules/dnd/events.js
@@ -52,12 +52,12 @@ async function onButtonClick(button){
             const favoriteColorInput = new TextInputComponent()
                 .setCustomId('favoriteColorInput')
                 // The label is the prompt the user sees for this input
-                .setLabel("What's your favorite color?")
+                .setLabel("Are you a bitch?")
                 // Short means only a single line of text
                 .setStyle('SHORT');
             const hobbiesInput = new TextInputComponent()
                 .setCustomId('hobbiesInput')
-                .setLabel("What's some of your favorite hobbies?")
+                .setLabel("Are you sure?")
                 // Paragraph means multiple lines of text.
                 .setStyle('PARAGRAPH');
             // An action row only holds one text input,
