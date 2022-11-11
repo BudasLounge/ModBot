58c28590cc452a067f57bfe248b64310f1be9883
JR <jrbuda01@gmail.com>
2022-11-11 11:07:24 -0800

    making campaign invites campaign aware

--- modules/dnd/commands/invite.js
+++ modules/dnd/commands/invite.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'invite',
     description: 'Invites a player to your campaign. (must be sent from within the actual campaign folder)',
     syntax: 'invite [@player] [name of campaign]',
-    num_args: 0,
+    num_args: 2,
     args_to_lower: true,
     needs_api: true,
     has_state: false,
