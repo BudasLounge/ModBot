0beb7a06e5dca34257d84b43b86677d13cf94ab0
JR <jrbuda01@gmail.com>
2022-11-11 12:34:41 -0800

    campaign creator modal setup

---
+++ modules/dnd/commands/campaigncreator.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'campaigncreator',
+    description: 'Initiates campaign creation for admin approval',
+    syntax: 'campaigncreator',
+    num_args: 0,
+    args_to_lower: true,
+    needs_api: true,
+    has_state: false,
+    async execute(message, args, extra) {
+        var api = extra.api;
+        const { Events, ModalBuilder } = require('discord.js');
+
+		const modal = new ModalBuilder()
+			.setCustomId('myModal')
+			.setTitle('My Modal');
+
+
+    }
+};
