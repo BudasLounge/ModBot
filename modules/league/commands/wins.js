848041307f33569c6d1b9d1179b3d2089591b498
JR <jrbuda01@gmail.com>
2023-05-04 10:21:17 -0700

    league wins losses command

--- modules/league/commands/wins.js
+++ modules/league/commands/wins.js
@@ -7,68 +7,35 @@ module.exports = {
     needs_api: false,
     has_state: false,
     async execute(message, args, extra) {
-        const request = require('request');
         const summonerName = args[1];
-        const region = 'americas';
         const apiKey = process.env.RIOT_API_KEY;
-
+        const {Client} = require('shieldbow')
         // Construct the URL for the match history request
-        const summonerIDget = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`;
-        request(summonerIDget, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return
-            }
-        const summoner = JSON.parse(body)
-        const matchHistoryUrl = `https://${region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${summoner.puuid}/ids?start=0&count=8&api_key=${apiKey}`;
-        message.reply(matchHistoryUrl)
 
-            // Make the HTTP request to retrieve the match history
-            request(matchHistoryUrl, (error, response, body) => {
-            if (error) {
-                console.error(error);
-            } else {
-                // Parse the JSON response body into a JavaScript object
-                const matchHistory = JSON.parse(body);
-                message.reply("MATCH DATA HERE:" + matchHistory)
-                // Initialize the win and loss counts to 0
-                let wins = 0;
-                let losses = 0;
+        const client = new Client(apiKey);
 
-                // Iterate through the matches to count the wins and losses
-                for (let i = 0; i < matchHistory.length; i++) {
-                const match = matchHistory[i];
-                message.reply(match)
-
-                // Construct the URL for the match details request
-                const matchDetailsUrl = `https://${region}.api.riotgames.com/lol/match/v5/matches/${match}?api_key=${apiKey}`;
-
-                // Make the HTTP request to retrieve the match details
-                request(matchDetailsUrl, (error, response, body) => {
-                    if (error) {
-                    console.error(error);
-                    } else {
-                    // Parse the JSON response body into a JavaScript object
-                    const matchDetails = JSON.parse(body);
-                    this.logger.info("MATCH DETAILS: " + matchDetails)
-                    // Determine whether the summoner won or lost the match
-                    const participantId = matchDetails.info.participants.find(participant => participant.summonerName.toLowerCase() === summonerName.toLowerCase()).participantId;
-                    this.logger.info("partID: "+participantId)
-                    const participant = matchDetails.info.participants.find(participant => participant.participantId === participantId);
-                    this.logger.info("part:"+participant)
-                    if (participant.win) {
-                        wins++;
-                    } else {
-                        losses++;
-                    }
-
-                    // Log the win/loss count after each match is processed
-                    message.reply(`Wins: ${wins}, Losses: ${losses}`);
-                    }
-                });
-                }
-            }
-            });
+        client
+        .initialize({
+            region: 'na', // defaults to 'na' anyways.
         })
+        .then(async () => {
+            // After initialization, you can use the client to make requests
+            // For example, lets fetch the following details of a summoner
+            // - Summoner name, summoner level
+            // - SoloQ ranking and LP
+            // - The highest champion mastery
+
+            const summoner = await client.summoners.fetchBySummonerName(summonerName);
+            const leagueEntry = await summoner.fetchLeagueEntries();
+            const championMastery = summoner.championMastery;
+
+            const soloQ = leagueEntry.get('RANKED_SOLO_5x5');
+            const highest = await championMastery.highest();
+
+            message.reply(`Summoner name: ${summoner.name} (level: ${summoner.level}).`);
+            message.reply(`SoloQ: ${soloQ.tier} ${soloQ.division} (${soloQ.lp} LP).`);
+            message.reply(`Highest champion mastery: ${highest.champion.name} (M${highest.level} ${highest.points} points).`);
+        });
+
     }
 };
