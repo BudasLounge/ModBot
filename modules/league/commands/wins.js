cb0814ff7af8fb8894c0ea14f298df8fb11047b0
JR <jrbuda01@gmail.com>
2023-05-04 11:38:42 -0700

    league wins losses command

--- modules/league/commands/wins.js
+++ modules/league/commands/wins.js
@@ -30,6 +30,7 @@ module.exports = {
             const championMastery = summoner.championMastery;
             const highest = await championMastery.highest();
             message.reply(`Summoner name: ${summoner.name} (level: ${summoner.level}).`);
+            message.reply(summoner)
             const soloQ = leagueEntry.get('RANKED_SOLO_5x5');
             if(soloQ){
                 message.reply(`SoloQ: ${soloQ.tier} ${soloQ.division} (${soloQ.lp} LP).`);
