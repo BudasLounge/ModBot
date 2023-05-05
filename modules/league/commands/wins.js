878a8c6274ee3903d4632ae03e17c674b7fa9573
JR <jrbuda01@gmail.com>
2023-05-05 09:24:02 -0700

    league wins now includes champion specific wins

--- modules/league/commands/wins.js
+++ modules/league/commands/wins.js
@@ -67,6 +67,8 @@ module.exports = {
                 this.logger.info("redTeam: " + redTeam)
                 const blueTeam = await matchInfo.teams.get("blue").participants
                 for(const player in redTeam){
+                    this.logger.info("player: " + player)
+                    this.logger.info("player: " + player.summoner.name)
                     if(player.summoner.name == summoner.name){
                         ourPlayer = player
                     }
