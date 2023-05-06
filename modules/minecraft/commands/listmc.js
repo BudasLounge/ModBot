22a7c1d44b84086ee10cb1c927dac483de13fd99
JR <jrbuda01@gmail.com>
2023-05-06 01:05:46 -0700

    league wins now includes champion specific wins, testing an optimized version

--- modules/minecraft/commands/listmc.js
+++ modules/minecraft/commands/listmc.js
@@ -1,4 +1,4 @@
-module.exports ={
+module.exports = {
     name: 'listmc',
     description: 'Shows all servers and their information',
     syntax: 'listmc',
@@ -6,38 +6,29 @@ module.exports ={
     args_to_lower: false,
     needs_api: true,
     has_state: false,
-    async execute(message, args, extra){
-        var api = extra.api;
-
-        const Discord = require('discord.js');
-        console.log(">>display_all_servers");
-        var respServer;
-        try{
-            respServer = await api.get("minecraft_server", {
-                _limit: 20
-            });
-        } catch(error){
-            console.error(error);
-        }
-        console.log(respServer.minecraft_servers.length + " servers found...");
-        var serverList = "List of all servers:\n\n";
-        const ListEmbed = new Discord.MessageEmbed()
-        .setColor("#f92f03")
-        .setTitle("List of all minecraft servers: ");
-        for(var i = 0;i<respServer.minecraft_servers.length;i++){
-            var nextItem = "";
-            nextItem += respServer.minecraft_servers[i].display_name +":\n";
-            nextItem += "short name: " + respServer.minecraft_servers[i].short_name + "\n";
-            nextItem += "server ip: " + respServer.minecraft_servers[i].server_ip + "\n";
-            nextItem += "numeric ip: " + respServer.minecraft_servers[i].numeric_ip + ":" + respServer.minecraft_servers[i].port + "\n";
-            nextItem += "minecraft version: " + respServer.minecraft_servers[i].mc_version + "\n";
-            nextItem += "pack version: " + respServer.minecraft_servers[i].pack_version + "\n";
-            nextItem += "date created: " + respServer.minecraft_servers[i].date_created + "\n";
-            serverList += nextItem + "\n";
-            ListEmbed.addField(respServer.minecraft_servers[i].display_name + " server info:", nextItem);
-        }
-        message.channel.send({ embeds: [ListEmbed]});
-        //message.channel.send({ content: serverList)
-        console.log("<<display_all_servers");
+    async execute(message, args, extra) {
+      const { api } = extra;
+  
+      this.logger.info('>>display_all_servers');
+      try {
+        const { minecraft_servers } = await api.get('minecraft_server', { _limit: 20 });
+        this.logger.info(`${minecraft_servers.length} servers found...`);
+  
+        const embed = new Discord.MessageEmbed()
+          .setColor('#f92f03')
+          .setTitle('List of all minecraft servers:');
+  
+        const fields = minecraft_servers.map(({ display_name, short_name, server_ip, numeric_ip, port, mc_version, pack_version, date_created }) => ({
+          name: `${display_name} server info:`,
+          value: `${display_name}:\nshort name: ${short_name}\nserver ip: ${server_ip}\nnumeric ip: ${numeric_ip}:${port}\nminecraft version: ${mc_version}\npack version: ${pack_version}\ndate created: ${date_created}\n\n`
+        }));
+  
+        embed.addFields(fields);
+  
+        message.channel.send({ embeds: [embed] });
+        this.logger.info('<<display_all_servers');
+      } catch (error) {
+        console.error(error);
+      }
     }
-};
+  };
