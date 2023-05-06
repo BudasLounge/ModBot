3caf9216f0895b8c3de27b445c40cf29c6465b2f
JR <jrbuda01@gmail.com>
2023-05-06 01:38:46 -0700

    league wins now includes champion specific wins, testing an optimized version

--- modules/minecraft/commands/status_all.js
+++ modules/minecraft/commands/status_all.js
@@ -8,15 +8,15 @@ module.exports = {
   has_state: false,
   async execute(message, args, { api }) {
     const { performance } = require('perf_hooks');
+    const perfStart = performance.now();
     const Discord = require('discord.js');
     const axios = require('axios');
     const { getStatus } = require('mc-server-status');
-    const perfStart = performance.now();
+    message.channel.send({ content: 'Let me get that for you... this might take a moment' });
     const respServer = await api.get('minecraft_server', { _limit: 20 }).catch((error) => {
       this.logger.error(error.response);
     });
     if (!respServer.minecraft_servers[0]) return;
-    message.channel.send({ content: 'Let me get that for you... this might take a moment' });
     const ListEmbed = new Discord.MessageEmbed().setColor('#f92f03').setTitle('List of all minecraft servers: ')
       .addField('Notice:\n', 'If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.');
     let stat_server = '';
