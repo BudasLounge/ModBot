656787ea0cc9cebff0fd21fc038afe7672505e58
JR <jrbuda01@gmail.com>
2023-07-12 00:49:58 -0700

    fixing minecraft status maybe

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -29,7 +29,7 @@ module.exports ={
             
             message.channel.send({content: respServer.minecraft_servers[0].numeric_ip + ":" + respServer.minecraft_servers[0].port})
             try{
-                item = await pinger.pingWithPromise('192.168.1.4', 36010).then(console.log).catch(console.error)
+                await pinger.pingWithPromise('192.168.1.4', 36010).then(response => {item = response}).catch(console.error)
                 this.logger.info("ITEM IN TRY: " + item)
             }catch(status_error){
                 this.logger.error(status_error.message);
