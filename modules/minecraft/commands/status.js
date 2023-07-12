e3471f43c236d2a3b1d0bff29780a30cd084b9d7
JR <jrbuda01@gmail.com>
2023-07-12 01:20:02 -0700

    fixing minecraft status maybe

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -24,16 +24,8 @@ module.exports ={
             .setColor("#f92f03")
             .setTitle(respServer.minecraft_servers[0].display_name + " status: ");
             ListEmbed.addField("Notice:\n","If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.")
-            try{
-                await pinger.pingWithPromise(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].port).then(response => {item = response}).catch(item = "OFFLINE")
-                this.logger.info("Item: " + item)
-            }catch(status_error){
-                this.logger.error(status_error.message);
-                item = respServer.minecraft_servers[0].display_name + " is currently offline!";
-                ListEmbed.addField("status: ", item);
-                message.channel.send({ embeds: [ListEmbed]});
-                flag = true;
-            }
+            await pinger.pingWithPromise(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].port).then(response => {item = response}).catch(item = "OFFLINE")
+            this.logger.info("Item: " + item)
             if(item != "OFFLINE"){
                 var output = respServer.minecraft_servers[0].display_name + " is currently online with: " + item.players.online + " players online!\n";
                 output += "Players online:\n";
@@ -48,7 +40,7 @@ module.exports ={
                 message.channel.send({ embeds: [ListEmbed]});
             }
         }else{
-            message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try /listmc for a list of all servers."});
+            message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers."});
         }
     }
 };
