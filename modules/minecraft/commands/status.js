6039ce263a1da1cf1624bdfd098656f0837199e8
JR <jrbuda01@gmail.com>
2023-07-12 01:22:25 -0700

    fixing minecraft status maybe

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -20,13 +20,22 @@ module.exports ={
         } 
         if(respServer.minecraft_servers[0]){
             var item;
+            var flag = false;
             const ListEmbed = new Discord.MessageEmbed()
             .setColor("#f92f03")
             .setTitle(respServer.minecraft_servers[0].display_name + " status: ");
             ListEmbed.addField("Notice:\n","If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.")
-            await pinger.pingWithPromise(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].port).then(response => {item = response}).catch(item = "OFFLINE")
-            this.logger.info("Item: " + item)
-            if(item != "OFFLINE"){
+            try{
+                await pinger.pingWithPromise(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].port).then(response => {item = response}).catch(item = "OFFLINE")
+                this.logger.info("Item: " + item)
+            }catch(status_error){
+                this.logger.error(status_error.message);
+                item = respServer.minecraft_servers[0].display_name + " is currently offline!";
+                ListEmbed.addField("status: ", item);
+                message.channel.send({ embeds: [ListEmbed]});
+                flag = true;
+            }
+            if(!flag){
                 var output = respServer.minecraft_servers[0].display_name + " is currently online with: " + item.players.online + " players online!\n";
                 output += "Players online:\n";
                 for(var i = 0;i<item.players.online;i++){
@@ -38,6 +47,7 @@ module.exports ={
                 item = respServer.minecraft_servers[0].display_name + " is currently offline!";
                 ListEmbed.addField("status: ", item);
                 message.channel.send({ embeds: [ListEmbed]});
+                flag = true;
             }
         }else{
             message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers."});
