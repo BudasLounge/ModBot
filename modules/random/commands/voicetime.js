49331e91bc1b18b384e353e337c03acfd9b34c59
JR <jrbuda01@gmail.com>
2023-07-15 01:25:14 -0700

    fixing voice timing

--- modules/random/commands/voicetime.js
+++ modules/random/commands/voicetime.js
@@ -7,7 +7,7 @@ module.exports = {
     needs_api: true,//if this command needs access to the api
     has_state: false,//if this command uses the state engine
     async execute(message, args, extra) {
-        var api = extra.api;
+        /*var api = extra.api;
         const {MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu} = require('discord.js');
         this.logger.info("Gathering all voice timings");
         try{
@@ -123,7 +123,127 @@ module.exports = {
         );
 
         message.channel.send({components: [timingFilters, timingFilters2], embeds: [ListEmbed]});
-        this.logger.info("Sent Voice Leaderboard!")
+        this.logger.info("Sent Voice Leaderboard!")*/
+        const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+        const api = extra.api;
+        this.logger.info("Gathering all voice timings");
+        try {
+        const respVoice = await api.get("voice_tracking", {
+            discord_server_id: message.guild.id,
+        });
+        } catch (error) {
+        this.logger.error(error);
+        }
+
+        const voiceTrackings = respVoice.voice_trackings;
+        if (!voiceTrackings[0]) {
+        message.channel.send({ content: "There is no data available yet..." });
+        return;
+        }
+
+        this.logger.info("Starting the additive loop");
+        const totalTime = new Map();
+        for (const track of voiceTrackings) {
+        const { username, connect_time, disconnect_time } = track;
+        const connectTime = parseInt(connect_time);
+        const disconnectTime = parseInt(disconnect_time) || Math.floor(new Date().getTime() / 1000);
+        const duration = Math.floor(disconnectTime - connectTime);
+
+        if (totalTime.has(username)) {
+            this.logger.info(`Adding to existing row: ${username}: ${duration}`);
+            totalTime.set(username, totalTime.get(username) + duration);
+        } else {
+            this.logger.info("Creating a new row.");
+            totalTime.set(username, duration);
+        }
+        }
+
+        this.logger.info("Printing array to a table, will only show up in live console, not logs...");
+        console.table([...totalTime]);
+        this.logger.info([...totalTime]);
+
+        const startDate = new Date(voiceTrackings[0].connect_time * 1000);
+        const formattedDate = startDate.toLocaleDateString('en-US', {
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+        });
+        const sortedTotalTime = [...totalTime.entries()].sort((a, b) => b[1] - a[1]).slice(0, 10);
+
+        const ListEmbed = new MessageEmbed()
+        .setColor("#c586b6")
+        .setTitle(`Voice Channel Leaderboard (Top 10) (Start Date: ${formattedDate})`);
+
+        for (let i = 0; i < sortedTotalTime.length; i++) {
+        const [username, duration] = sortedTotalTime[i];
+        let diff = duration;
+        const units = [
+            { d: 60, l: "seconds" },
+            { d: 60, l: "minutes" },
+            { d: 24, l: "hours" },
+            // change 365 to a higher number if someone hits 365 days of cumulative voice timings
+            { d: 1000, l: "days" },
+        ];
+
+        let s = '';
+        for (let j = 0; j < units.length; ++j) {
+            s = `${diff % units[j].d} ${units[j].l} ${s}`;
+            diff = Math.floor(diff / units[j].d);
+        }
+        ListEmbed.addField(`${i + 1}. ${username}`, s);
+        }
+
+        const timingFilters = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId("non-muted")
+            .setLabel("Non-muted times only")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+            new MessageButton()
+            .setCustomId("muted")
+            .setLabel("Muted times only")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+            new MessageButton()
+            .setCustomId("lonely")
+            .setLabel("Alone times only")
+            .setStyle('PRIMARY')
+            .setDisabled(true),
+            new MessageButton()
+            .setCustomId("bottom")
+            .setLabel("Bottom Talkers")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+        );
+
+        const timingFilters2 = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId("30days")
+            .setLabel("Top - Last 30 Days")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+            new MessageButton()
+            .setCustomId("7days")
+            .setLabel("Top - Last 7 Days")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+            new MessageButton()
+            .setCustomId("channel")
+            .setLabel("Top Talkers - By Channel")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+            new MessageButton()
+            .setCustomId("channelUse")
+            .setLabel("Top Channels by use")
+            .setStyle('PRIMARY')
+            .setDisabled(false),
+        );
+
+        message.channel.send({ components: [timingFilters, timingFilters2], embeds: [ListEmbed] });
+        this.logger.info("Sent Voice Leaderboard!");
     }
 }
 
