6f900e2f3cf9f3e877ffcc7da7bdf70bcc987105
JR <jrbuda01@gmail.com>
2023-07-15 01:54:06 -0700

    fixing voice timing

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -7,7 +7,7 @@ module.exports ={
     needs_api: true,
     has_state: false,
     async execute(message, args, extra){
-        var api = extra.api;
+        /*var api = extra.api;
         const Discord = require('discord.js');
         const pinger = require("minecraft-ping-js");
         var respServer;
@@ -46,6 +46,43 @@ module.exports ={
             }
         }else{
             message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers."});
+        }*/
+        const Discord = require('discord.js');
+        const pinger = require("minecraft-ping-js");
+
+        const api = extra.api;
+
+        try {
+        const respServer = await api.get("minecraft_server", {
+            short_name: args[1]
+        });
+
+        if (respServer.minecraft_servers[0]) {
+            const server = respServer.minecraft_servers[0];
+            const ListEmbed = new Discord.MessageEmbed()
+            .setColor("#f92f03")
+            .setTitle(server.display_name + " status: ")
+            .addField("Notice:\n", "If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.");
+
+            try {
+            const response = await pinger.pingWithPromise(server.numeric_ip, server.port);
+            const item = response || "OFFLINE";
+            const output = server.display_name + " is currently online with: " + item.players.online + " players online!\nPlayers online:\n" +
+                item.players.sample.map(player => "- " + player.name).join("\n");
+
+            ListEmbed.addField("status: ", output);
+            message.channel.send({ embeds: [ListEmbed] });
+            } catch (status_error) {
+            this.logger.error(status_error.message);
+            const item = server.display_name + " is currently offline!";
+            ListEmbed.addField("status: ", item);
+            message.channel.send({ embeds: [ListEmbed] });
+            }
+        } else {
+            message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers." });
+        }
+        } catch (error) {
+        this.logger.error(error);
         }
     }
 };
