dea88c140b0f71bcd84d3eb9eec860e9e0631fb7
JR <jrbuda01@gmail.com>
2023-07-16 22:03:16 -0700

    updating game menu

--- modules/random/events.js
+++ modules/random/events.js
@@ -1158,7 +1158,7 @@ async function onButtonClick(button){
                     for(var i = 0;i<respPlayersList.game_joining_players.length;i++){
                         playersList += "<@" + respPlayersList.game_joining_players[i].player_id + ">\n";
                         kickableList.addOptions({
-                            label: respPlayersList.game_joining_players[i].player_id,
+                            label: "<@" + respPlayersList.game_joining_players[i].player_id + ">",
                             value: respPlayersList.game_joining_players[i].player_id,
                             description: "Kick " + respPlayersList.game_joining_players[i].player_id + " from the game",
                             emoji: '👢',
