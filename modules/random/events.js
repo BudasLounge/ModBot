a2e6f2e875497af7828309d7f73fbf2c6ed5dc5d
JR <jrbuda01@gmail.com>
2023-07-16 22:47:38 -0700

    updating game menu

--- modules/random/events.js
+++ modules/random/events.js
@@ -136,7 +136,7 @@ async function onButtonClick(button){
             }
             var flag = false;
             for(var j = 0;j<totalTime.length;j++){
-                if(totalTime[j][0] == respVoice.voice_trackings[i].username){
+                if(totalTime[j][0] == respVoice.voice_trackings[i].user_id){
                     //logger\.info\("Adding to existing row\."\)
                     totalTime[j][1] += Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))
                     flag = true;
@@ -145,7 +145,7 @@ async function onButtonClick(button){
             }
             if(!flag){
                 logger.info("Creating a new row.")
-                totalTime.push([respVoice.voice_trackings[i].username, Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))])
+                totalTime.push([respVoice.voice_trackings[i].user_id, Math.floor(parseInt(respVoice.voice_trackings[i].disconnect_time) - parseInt(respVoice.voice_trackings[i].connect_time))])
             }
         }
         logger.info("Printing array to a table, will only show up in live console, not logs...")
