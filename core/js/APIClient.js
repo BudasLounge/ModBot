4d6696029146ca402079e962af447ffe6a00e69b
JR <jrbuda01@gmail.com>
2023-09-26 16:14:45 -0700

    trying to fix needing to hit the internet for the bot to work with the API

--- core/js/APIClient.js
+++ core/js/APIClient.js
@@ -15,7 +15,7 @@ class APIClient {
 	 * access token. Once the APIClient has a token, it will fire an 'ApiReady' event on document.
 	 */
 	constructor() {
-        this.api_url = "https://budaslounge.com/api/";
+        this.api_url = "192.168.1.9/api/";
 		this.access_url = "ModBot Server";
 		this.token = fs.readFileSync(__dirname + "/../../../api_token.txt", "utf8");
 	}
