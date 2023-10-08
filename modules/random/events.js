7ae781e0155e2752c80c5f34fee021ed0a18333f
JR <jrbuda01@gmail.com>
2023-10-08 03:25:27 -0700

    updating game command

--- modules/random/events.js
+++ modules/random/events.js
@@ -1934,7 +1934,8 @@ async function onButtonClick(button){
                             game_id:parseInt(respGame.game_joining_masters[0].game_id),
                             player_id:captain1,
                             captain:"yes",
-                            game_player_id:parseInt(newCaptain1)
+                            game_player_id:parseInt(newCaptain1),
+                            team:"1"
                         })
                     }catch(error){
                         logger.error(error.message);
