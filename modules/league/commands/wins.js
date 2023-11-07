b61f8ea17e678d4681a2a233d01bcdadcc9a5100
JR <jrbuda01@gmail.com>
2023-11-06 19:58:43 -0800

    allowed wins command to be used again

--- modules/league/commands/wins.js
+++ modules/league/commands/wins.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'wins',
     description: 'Shows last 20 games in your match history',
-    syntax: 'wins [summoner name] ',
+    syntax: 'wins [summoner name]',
     num_args: 1,
     args_to_lower: true,
     needs_api: false,
@@ -12,23 +12,21 @@ module.exports = {
         const axios = require('axios');
         const rateLimit = require('axios-rate-limit');
 
-        const RIOT_API_KEY = process.env.RIOT_API_KEY; // Your Riot API Key should be in a .env file
-        const RIOT_ACCOUNT_BASE_URL = 'https://na1.api.riotgames.com/lol'
-        const RIOT_API_BASE_URL = 'https://americas.api.riotgames.com/lol'; // Replace REGION with the appropriate region code
+        const RIOT_API_KEY = process.env.RIOT_API_KEY;
+        const RIOT_ACCOUNT_BASE_URL = 'https://na1.api.riotgames.com/lol';
+        const RIOT_API_BASE_URL = 'https://americas.api.riotgames.com/lol';
 
-        // Set up rate limiting according to the provided limits
         const http = rateLimit(axios.create(), {
             maxRequests: 20,
             perMilliseconds: 1000
         });
 
-        // Also set up a longer term rate limit
         let longTermRequests = 0;
-        const LONG_TERM_LIMIT = 100; // 100 requests
-        const LONG_TERM_DURATION = 120 * 1000; // 2 minutes in milliseconds
+        const LONG_TERM_LIMIT = 100;
+        const LONG_TERM_DURATION = 120 * 1000;
 
         setInterval(() => {
-        longTermRequests = 0; // Reset the long term request count every 2 minutes
+            longTermRequests = 0;
         }, LONG_TERM_DURATION);
 
         http.interceptors.request.use(config => {
@@ -42,70 +40,76 @@ module.exports = {
             return Promise.reject(error);
         });
 
+        http.interceptors.response.use(response => {
+            return response;
+        }, async (error) => {
+            if (error.response && error.response.status === 429) {
+                const retryAfter = error.response.headers['retry-after'] ? parseInt(error.response.headers['retry-after']) : 1;
+                console.log(`Rate limit exceeded. Retrying after ${retryAfter} seconds.`);
+                await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
+                return http.request(error.config);
+            }
+            return Promise.reject(error);
+        });
+
         async function getLast20Matches(username) {
             try {
-                this.logger.info('username: ' + username)
-                // Step 1: Get the summoner details to retrieve PUUID
                 const summonerResponse = await http.get(`${RIOT_ACCOUNT_BASE_URL}/summoner/v4/summoners/by-name/${encodeURIComponent(username)}`, {
                     headers: {
                         "X-Riot-Token": RIOT_API_KEY
                     }
                 });
                 const { puuid } = summonerResponse.data;
-                this.logger.info('puuid: ' + puuid)
-                // Step 2: Get the list of match IDs for the summoner
+
                 const matchIdsResponse = await http.get(`${RIOT_API_BASE_URL}/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`, {
                     headers: {
                         "X-Riot-Token": RIOT_API_KEY
                     }
                 });
-                await new Promise(resolve => setTimeout(resolve, 1000));
                 const matchIds = matchIdsResponse.data;
-                this.logger.info('matchIds: ' + matchIds)
-                // Step 3: Get match details and determine wins/losses for each match ID
+
                 const results = await Promise.all(matchIds.map(async (matchId) => {
                     const matchDetailResponse = await http.get(`${RIOT_API_BASE_URL}/match/v5/matches/${matchId}`, {
                         headers: {
                             "X-Riot-Token": RIOT_API_KEY
                         }
                     });
-        
+
                     const matchDetail = matchDetailResponse.data;
                     const participant = matchDetail.info.participants.find(p => p.puuid === puuid);
-        
+
                     return {
                         matchId: matchId,
-                        champion: participant.championName, // v5 API provides champion names directly
+                        champion: participant.championName,
                         win: participant.win
                     };
                 }));
-                this.logger.info('results: ' + JSON.stringify(results))
+
                 return results;
             } catch (error) {
-                console.error('Error fetching data from Riot API:', error.message);
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.error('Response data:', error.response.data);
-                    console.error('Response status:', error.response.status);
-                    console.error('Response headers:', error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    console.error('Request:', error.request);
+                if (longTermRequests >= LONG_TERM_LIMIT) {
+                    message.channel.send(`The rate limit of ${LONG_TERM_LIMIT} requests per ${LONG_TERM_DURATION / 1000 / 60} minutes has been exceeded. Please wait 2 minutes before trying again.`);
                 } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.error('Error message:', error.message);
+                    this.logger.error('Error fetching data from Riot API:', error.message);
+                    if (error.response) {
+                        this.logger.error('Response data:', error.response.data);
+                        this.logger.error('Response status:', error.response.status);
+                        this.logger.error('Response headers:', error.response.headers);
+                    } else if (error.request) {
+                        this.logger.error('Request:', error.request);
+                    } else {
+                        this.logger.error('Error message:', error.message);
+                    }
+                    message.channel.send('An error occurred while retrieving match history.');
                 }
-                throw error;
             }
         }
 
-        // Usage
         getLast20Matches(args[1]).then(results => {
             let response = 'Last 20 matches:\n';
             results.forEach(result => {
                 const winLoss = result.win ? 'Win' : 'Loss';
-                response += `Game ID: ${result.gameId}, Champion ID: ${result.champion}, Result: ${winLoss}\n`;
+                response += `Game ID: ${result.matchId}, Champion: ${result.champion}, Result: ${winLoss}\n`;
             });
             message.channel.send(response);
         }).catch(error => {
@@ -113,5 +117,4 @@ module.exports = {
             message.channel.send('An error occurred while retrieving match history.');
         });
     }
-    
 };
