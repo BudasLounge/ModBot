a5aed04c0e59c63d25536e782e1d2833e865d3ff
JR <jrbuda01@gmail.com>
2023-11-06 20:01:00 -0800

    allowed wins command to be used again

--- modules/league/commands/wins.js
+++ modules/league/commands/wins.js
@@ -67,7 +67,9 @@ module.exports = {
                     }
                 });
                 const matchIds = matchIdsResponse.data;
-
+                setTimeout(() => {
+                    // code to execute after 1 second
+                }, 1000);
                 const results = await Promise.all(matchIds.map(async (matchId) => {
                     const matchDetailResponse = await http.get(`${RIOT_API_BASE_URL}/match/v5/matches/${matchId}`, {
                         headers: {
