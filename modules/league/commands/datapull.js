99a0c69d23e9ceffcc4554ed11a2da0626d3623b
JR <jrbuda01@gmail.com>
2023-11-11 01:55:59 -0800

    testing api outputs

--- modules/league/commands/datapull.js
+++ modules/league/commands/datapull.js
@@ -4,7 +4,7 @@ module.exports = {
     syntax: 'datapull [summoner name] [number of games up to 1000](optional)',
     num_args: 2,
     args_to_lower: true,
-    needs_api: false,
+    needs_api: true,
     has_state: false,
     
     async execute(message, args, extra) {
