e13d69bd3669b467c904f081e5d162927ae4a348
JR <jrbuda01@gmail.com>
2024-01-03 15:41:43 -0800

    added field to listmc

--- modules/minecraft/commands/listmc.js
+++ modules/minecraft/commands/listmc.js
@@ -14,8 +14,8 @@ module.exports ={
         const ListEmbed = new Discord.MessageEmbed()
             .setColor("#f92f03")
             .setTitle("List of all minecraft servers: ");
-        respServer.minecraft_servers.map(({ display_name, short_name, server_ip, numeric_ip, port, mc_version, pack_version, date_created }) => {
-            const nextItem = `${display_name}:\nshort name: ${short_name}\nserver ip: ${server_ip}\nnumeric ip: ${numeric_ip}:${port}\nminecraft version: ${mc_version}\npack version: ${pack_version}\ndate created: ${date_created}\n`;
+        respServer.minecraft_servers.map(({ display_name, short_name, server_ip, numeric_ip, port, mc_version, pack_version, date_created, url }) => {
+            const nextItem = `${display_name}:\nshort name: ${short_name}\nserver ip: ${server_ip}\nnumeric ip: ${numeric_ip}:${port}\nminecraft version: ${mc_version}\npack version: ${pack_version}\ndate created: ${date_created}\nurl: ${url}\n`;
             ListEmbed.addField(`${display_name} server info:`, nextItem);
         });
         message.channel.send({ embeds: [ListEmbed] });
