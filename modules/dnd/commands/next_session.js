7de54731034e1e2539072fdaaefadf6ecdaafadc
JR <jrbuda01@gmail.com>
2024-01-17 09:11:10 -0800

    automating DnD scheduling messages

--- modules/dnd/commands/next_session.js
+++ modules/dnd/commands/next_session.js
@@ -108,7 +108,7 @@ module.exports = {
                 // Parse the date-time string into a JavaScript Date object
                 const dateTimestamp = new Date(respDndSession.dnd_campaigns[0].next_session);
                 // Schedule the job
-                schedule.scheduleJob(respDndSession.dnd_campaigns[0].module+"-COMMAND", dateTimestamp, async function() {
+                await schedule.scheduleJob(respDndSession.dnd_campaigns[0].module+"-COMMAND", dateTimestamp, async function() {
                     try{
                         logger.info(`Sending message for session ${respDndSession.dnd_campaigns[0].module}`);
                         const guild = await global.client.guilds.fetch('650865972051312673');
