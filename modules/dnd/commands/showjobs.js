e5a25fa2ae2dc5f607b06d81c96bab3782db79d0
JR <jrbuda01@gmail.com>
2024-07-10 00:38:57 -0700

    making the scheduled message portion better

--- modules/dnd/commands/showjobs.js
+++ modules/dnd/commands/showjobs.js
@@ -1,25 +1,29 @@
+const schedule = require('node-schedule');
+
 module.exports = {
     name: 'showjobs',
     description: 'Show any scheduled jobs',
     syntax: 'showjobs',
-    num_args: 0,//minimum amount of arguments to accept
-    args_to_lower: false,//if the arguments should be lower case
-    needs_api: true,//if this command needs access to the api
-    has_state: false,//if this command uses the state engine
+    num_args: 0, // minimum amount of arguments to accept
+    args_to_lower: false, // if the arguments should be lower case
+    needs_api: true, // if this command needs access to the API
+    has_state: false, // if this command uses the state engine
     async execute(message, args, extra) {
-        const schedule = require('node-schedule');
         const jobs = schedule.scheduledJobs;
         const jobNames = Object.keys(jobs);
 
         if (jobNames.length === 0) {
-            return 'No scheduled jobs.';
+            await message.reply('No scheduled jobs.');
+            return;
         }
 
         let jobList = 'All scheduled jobs:\n';
         jobNames.forEach(name => {
             const job = jobs[name];
-            jobList += `Job Name: ${name}, Next Invocation: ${job.nextInvocation().toString()}\n`;
+            const nextInvocation = job.nextInvocation();
+            jobList += `Job Name: ${name}, Next Invocation: ${nextInvocation ? nextInvocation.toString() : 'No next invocation'}\n`;
         });
+
         await message.reply(jobList);
     }
-}
+};
