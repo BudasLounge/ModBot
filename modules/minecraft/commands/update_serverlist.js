06d68e70edf5e4e1ab109904bea3b71c27e05575
JR <jrbuda01@gmail.com>
2024-09-04 12:20:01 -0700

    updated the updateSL command for MC

--- modules/minecraft/commands/update_serverlist.js
+++ modules/minecraft/commands/update_serverlist.js
@@ -1,59 +1,74 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
   name: 'updatesl',
-  description: 'Used to update parts of the minecraft server list',
+  description: 'Used to update parts of the Minecraft server list',
   syntax: 'updatesl [server name] [whats updating] [new value]',
   num_args: 3,
   args_to_lower: true,
   needs_api: true,
   has_state: false,
+  
   async execute(message, args, extra) {
     const api = extra.api;
-    const Discord = require('discord.js');
-    const authorizedUser = message.member.roles.cache.find(r => r.id === '586313447965327365') || message.author.id === '185223223892377611';
+
+    const authorizedUser = message.member.roles.cache.has('586313447965327365') || message.author.id === '185223223892377611';
     if (!authorizedUser) {
       return message.channel.send({ content: 'You don\'t have permission to use that command!' });
     }
-    args.shift();
-    const [shortName, fieldToUpdate, newValue] = args;
-    let respServer;
+
+    const [shortName, fieldToUpdate, newValue] = args.slice(1);
+
     try {
-      respServer = await api.get('minecraft_server', { short_name: shortName });
-      if (!respServer.minecraft_servers[0]) {
-        respServer = await api.get('minecraft_server', { display_name: shortName });
-        if (!respServer.minecraft_servers[0]) {
-          return message.channel.send({ content: 'No server with that short_hand or display_name.' });
+      let { minecraft_servers: [server] } = await api.get('minecraft_server', { short_name: shortName });
+
+      if (!server) {
+        ({ minecraft_servers: [server] } = await api.get('minecraft_server', { display_name: shortName }));
+        if (!server) {
+          return message.channel.send({ content: 'No server found with that short_name or display_name.' });
         }
       }
-      const data = { short_name: respServer.minecraft_servers[0].short_name };
-      data[fieldToUpdate] = newValue;
-      const respUpdate = await api.put('minecraft_server', data);
-      if (respUpdate.ok == true) {
-        const changedInfo = `
-            short_name: ${respServer.minecraft_servers[0].short_name}
-            display_name: ${respServer.minecraft_servers[0].display_name}
-            server_ip + port: ${respServer.minecraft_servers[0].server_ip}
-            numeric_ip: ${respServer.minecraft_servers[0].numeric_ip}:${respServer.minecraft_servers[0].port}
-            status_api_port: ${respServer.minecraft_servers[0].status_api_port}
-            mc_version: ${respServer.minecraft_servers[0].mc_version}
-            pack_version: ${respServer.minecraft_servers[0].pack_version}
-            vvvvv has been changed to vvvvv
-            short_name: ${respUpdate.minecraft_server.short_name}
-            display_name: ${respUpdate.minecraft_server.display_name}
-            server_ip + port: ${respUpdate.minecraft_server.server_ip}
-            numeric_ip: ${respUpdate.minecraft_server.numeric_ip}:${respUpdate.minecraft_server.port}
-            status_api_port: ${respUpdate.minecraft_server.status_api_port}
-            mc_version: ${respUpdate.minecraft_server.mc_version}
-            pack_version: ${respUpdate.minecraft_server.pack_version}`;
 
-        const listEmbed = new Discord.MessageEmbed()
+      const data = { short_name: server.short_name, [fieldToUpdate]: newValue };
+      const respUpdate = await api.put('minecraft_server', data);
+
+      if (respUpdate.ok) {
+        const updatedServer = respUpdate.minecraft_server;
+
+        // Creating the information in a cleaner, tabular style
+        const changedInfo = `
+**Before Update:**
+• **Short Name**: ${server.short_name}
+• **Display Name**: ${server.display_name}
+• **IP + Port**: ${server.server_ip}:${server.port}
+• **Numeric IP**: ${server.numeric_ip}:${server.port}
+• **Status API Port**: ${server.status_api_port}
+• **MC Version**: ${server.mc_version}
+• **Pack Version**: ${server.pack_version}
+
+**After Update:**
+• **Short Name**: ${updatedServer.short_name}
+• **Display Name**: ${updatedServer.display_name}
+• **IP + Port**: ${updatedServer.server_ip}:${updatedServer.port}
+• **Numeric IP**: ${updatedServer.numeric_ip}:${updatedServer.port}
+• **Status API Port**: ${updatedServer.status_api_port}
+• **MC Version**: ${updatedServer.mc_version}
+• **Pack Version**: ${updatedServer.pack_version}
+`;
+
+        const embed = new MessageEmbed()
           .setColor('#f92f03')
-          .setTitle('Here\'s what changed: \n' + fieldToUpdate)
-          .addField('A post function update: ', changedInfo);
-        message.channel.send({ embeds: [listEmbed] });
+          .setTitle(`Field Updated: ${fieldToUpdate}`)
+          .setDescription(changedInfo)
+          .setFooter('Minecraft Server List Update');
+
+        return message.channel.send({ embeds: [embed] });
       }
+
+      return message.channel.send({ content: 'Update failed. Please try again.' });
     } catch (err) {
-      console.error(err);
-      message.channel.send({ content: 'An error occurred.' });
+      console.error('Error during update:', err);
+      return message.channel.send({ content: 'An error occurred while processing your request.' });
     }
   }
 };
