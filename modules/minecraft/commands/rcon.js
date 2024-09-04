3ab95ef42146ce7a8b3e0c4621a12ab28057bb2f
JR <jrbuda01@gmail.com>
2024-09-04 12:29:44 -0700

    updated the rcon command for MC

--- modules/minecraft/commands/rcon.js
+++ modules/minecraft/commands/rcon.js
@@ -1,70 +1,81 @@
+const fs = require('fs');
+const Rcon = require('rcon');
+
 module.exports = {
     name: 'rcon',
-    description: 'pushes an rcon command to a minecraft server',
+    description: 'Pushes an rcon command to a Minecraft server',
     syntax: 'rcon [minecraft_shortname] [rcon_command/minecraft_server_command]',
-    num_args: 1,//minimum amount of arguments to accept
-    args_to_lower: true,//if the arguments should be lower case
-    needs_api: true,//if this command needs access to the api
-    has_state: false,//if this command uses the state engine
+    num_args: 2, // minimum number of arguments required
+    args_to_lower: true, // arguments should be lower case
+    needs_api: true, // if this command needs access to the API
+    has_state: false, // if this command uses the state engine
     async execute(message, args, extra) {
-        var api = extra.api
-        var fs = require('fs');
-        var password = fs.readFileSync("../rcon_password.txt").toString();
-        password = password.replace(/\s+/g, '');
-        var Rcon = require('rcon');
+        const api = extra.api;
 
-
-        if(args[1]==="help"){
-            message.reply({ content: "Arguments:\nminecraft_shortname: the short name of the minecraft server. Typically the first part of their IP, can be found with ,listmc\nrcon_command: any in-game server command that would usually follow a '/'\nMUST BE LISTED AS AN MC ADMIN TO USE THIS COMMAND"});
-            return
+        // Read RCON password and sanitize it
+        let password;
+        try {
+            password = fs.readFileSync('../rcon_password.txt').toString().trim();
+        } catch (err) {
+            console.error('Error reading RCON password:', err);
+            return message.reply({ content: 'Unable to read RCON password. Please check the configuration.' });
         }
 
-        if(!message.member.roles.cache.some(role => role.name === "MCadmin")){
-            message.reply({content: "You are not an MC Admin, you cannot use this command."})
-            return;
+        // Display help information
+        if (args[1] === 'help') {
+            return message.reply({
+                content: "Arguments:\n`minecraft_shortname`: the short name of the Minecraft server (first part of the IP, use ,listmc to find).\n`rcon_command`: any in-game server command, usually prefixed with `/`.\nMUST BE LISTED AS AN MC ADMIN TO USE THIS COMMAND."
+            });
         }
 
-        if(!args[2]){
-            message.reply({ content: "Please make sure to fill out all arguments. Use 'rcon help' to see how to use it!"});
-            return;
+        // Ensure the user has the required role
+        if (!message.member.roles.cache.some(role => role.name === 'MCadmin')) {
+            return message.reply({ content: "You are not an MC Admin, so you cannot use this command." });
         }
 
-        var respServer;
-            try{
-                respServer = await api.get("minecraft_server", {
-                    short_name: args[1]
-                });
-            } catch(error){
-                this.logger.error(error.message);
-            } 
-        if(!respServer.minecraft_servers[0]){
-            message.reply({ content: "No server with that shortname exists, use ,listmc to find one!"});
-            return;
+        // Ensure all arguments are provided
+        if (!args[2]) {
+            return message.reply({ content: "Please provide all required arguments. Use `rcon help` for instructions." });
         }
-        var command;
-        if(args[3]){
-            args.shift()
-            args.shift()
-            command = args.join(' ');
-        }else{
-            command = args[2]
+
+        // Fetch the server information from the API
+        let respServer;
+        try {
+            respServer = await api.get('minecraft_server', { short_name: args[1] });
+        } catch (error) {
+            console.error('Error fetching server data:', error.message);
+            return message.reply({ content: 'Error retrieving server information. Please try again.' });
         }
-        //this.logger.info("Password: " + password);
-        var conn = new Rcon(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].rcon_port, password);
-        //message.reply({content: "Sending command to server! There will not be a response on if the command was successful."})
+
+        // Check if the server exists
+        const server = respServer.minecraft_servers && respServer.minecraft_servers[0];
+        if (!server) {
+            return message.reply({ content: "No server found with that short name. Use `,listmc` to find a valid server." });
+        }
+
+        // Construct the RCON command
+        const command = args.slice(2).join(' ');
+
+        // Establish RCON connection
+        const conn = new Rcon(server.numeric_ip, server.rcon_port, password);
+
+        // RCON connection events
         conn.on('auth', function() {
-            console.log("Authenticated");
-            console.log("Sending command:" + command)
+            console.log('RCON authenticated.');
+            console.log('Sending command:', command);
             conn.send(command);
         }).on('response', function(str) {
-            console.log("Response: " + str);
-            message.reply({content: "Command executed successfully!"}); // Notify user
+            console.log('RCON response:', str);
+            message.reply({ content: `Command executed successfully:\n${str}` }); // Notify user with response
         }).on('error', function(err) {
-            console.log("Error: " + err);
-            message.reply({content: "An error occurred while executing the command."}); // Notify user
+            console.error('RCON error:', err);
+            message.reply({ content: 'An error occurred while executing the command.' });
         }).on('end', function() {
-            console.log("Connection closed");
-            process.exit();
+            console.log('RCON connection closed.');
+            conn.disconnect(); // Close connection gracefully without exiting the process
         });
+
+        // Connect to RCON
+        conn.connect();
     }
-}
+};
