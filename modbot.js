fa767a70aa0d8e1491b3454915e0683ba2e45f04
JR <jrbuda01@gmail.com>
2024-09-30 14:00:20 -0700

    ignoring the package files

--- modbot.js
+++ modbot.js
@@ -126,7 +126,6 @@ function authClient() {
 
 client.on('messageCreate', (message) => {
     logger.info("Got message!");
-    
     //Add ability to check first time someone sends a message (not command) and grant them points
     modules.handle_command(message);
 });
