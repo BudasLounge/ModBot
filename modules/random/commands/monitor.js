98ce3de732337ee5337228ec2f50d5e3a145e49b
JR <jrbuda01@gmail.com>
2025-01-30 05:51:36 -0800

    lowered time to 1s instead of 5s in the monitor command

--- modules/random/commands/monitor.js
+++ modules/random/commands/monitor.js
@@ -35,7 +35,7 @@ module.exports = {
                     headers: {
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
                     },
-                    timeout: 5000, // 5 seconds timeout
+                    timeout: 1000, // 5 seconds timeout
                 });
 
                 const html = response.data;
