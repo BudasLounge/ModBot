546dd926d2f7220c636320ebea26340973467fd5
JR <jrbuda01@gmail.com>
2025-05-04 16:10:55 -0700

    updating status command to look better and function correctly

--- modules/minecraft/commands/status.js
+++ modules/minecraft/commands/status.js
@@ -1,4 +1,4 @@
-module.exports ={
+module.exports = {
     name: 'status',
     description: 'Finds the status of a minecraft server',
     syntax: 'status [name of server]',
@@ -6,83 +6,93 @@ module.exports ={
     args_to_lower: true,
     needs_api: true,
     has_state: false,
-    async execute(message, args, extra){
-        /*var api = extra.api;
-        const Discord = require('discord.js');
-        const pinger = require("minecraft-ping-js");
-        var respServer;
-        try{
-            respServer = await api.get("minecraft_server", {
-                short_name: args[1]
-            });
-        } catch(error){
-            this.logger.error(error);
-        } 
-        if(respServer.minecraft_servers[0]){
-            var item;
-            var flag = false;
-            const ListEmbed = new Discord.MessageEmbed()
-            .setColor("#f92f03")
-            .setTitle(respServer.minecraft_servers[0].display_name + " status: ");
-            ListEmbed.addField("Notice:\n","If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.")
-            try{
-                await pinger.pingWithPromise(respServer.minecraft_servers[0].numeric_ip, respServer.minecraft_servers[0].port).then(response => {item = response}).catch(item = "OFFLINE")
-                this.logger.info("Item: " + item)
-            }catch(status_error){
-                this.logger.error(status_error.message);
-                item = respServer.minecraft_servers[0].display_name + " is currently offline!";
-                ListEmbed.addField("status: ", item);
-                message.channel.send({ embeds: [ListEmbed]});
-                flag = true;
-            }
-            if(!flag){
-                var output = respServer.minecraft_servers[0].display_name + " is currently online with: " + item.players.online + " players online!\n";
-                output += "Players online:\n";
-                for(var i = 0;i<item.players.online;i++){
-                    output += "- " + item.players.sample[i].name + "\n";
-                }
-                ListEmbed.addField("status: ", output);
-                message.channel.send({ embeds: [ListEmbed]});
-            }
-        }else{
-            message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers."});
-        }*/
+    async execute(message, args, extra) {
+        const { performance } = require('perf_hooks');
+        const perfStart = performance.now();
         const Discord = require('discord.js');
         const pinger = require("minecraft-ping-js");
 
         const api = extra.api;
 
+        message.channel.send({ content: 'Checking server status...' });
+
         try {
-        const respServer = await api.get("minecraft_server", {
-            short_name: args[1]
-        });
+            const respServer = await api.get("minecraft_server", {
+                short_name: args[1]
+            });
 
-        if (respServer.minecraft_servers[0]) {
-            const server = respServer.minecraft_servers[0];
-            const ListEmbed = new Discord.MessageEmbed()
-            .setColor("#f92f03")
-            .setTitle(server.display_name + " status: ")
-            .addField("Notice:\n", "If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.");
+            if (respServer.minecraft_servers[0]) {
+                const server = respServer.minecraft_servers[0];
+                const ListEmbed = new Discord.MessageEmbed()
+                    .setColor("#f92f03")
+                    .setTitle(`${server.display_name} Status`)
+                    .setDescription(`IP: \`${server.numeric_ip}\` | Port: \`${server.port || 25565}\``)
+                    .addField("Notice:", "If the server crashed, it should auto restart in 5 minutes or less\nContact a server admin if it does not.");
 
-            try {
-            const response = await pinger.pingWithPromise(server.numeric_ip, server.port);
-            const item = response || "OFFLINE";
-            const output = server.display_name + " is currently online with: " + item.players.online + " players online!\nPlayers online:\n" +
-                item.players.sample.map(player => "- " + player.name).join("\n");
+                try {
+                    const response = await pinger.pingWithPromise(server.numeric_ip, server.port);
+                    
+                    if (response) {
+                        ListEmbed.addField("Status:", "✅ **ONLINE**", true);
+                        ListEmbed.addField("Players:", `${response.players.online}/${response.players.max}`, true);
+                        
+                        if (response.version) {
+                            ListEmbed.addField("Version:", response.version.name, true);
+                        }
+                        
+                        if (response.motd && response.motd.clean) {
+                            ListEmbed.addField("MOTD:", response.motd.clean);
+                        }
+                        
+                        // Handle player list with special character escaping
+                        if (response.players.online > 0 && response.players.sample) {
+                            const SensitiveCharacters = ['\\', '*', '_', '~', '`', '|', '>'];
+                            let playersList = "";
+                            
+                            for (const player of response.players.sample) {
+                                let escapedName = player.name;
+                                for (const unsafe of SensitiveCharacters) {
+                                    escapedName = escapedName.replaceAll(unsafe, `\\${unsafe}`);
+                                }
+                                playersList += `- ${escapedName}\n`;
+                            }
+                            
+                            ListEmbed.addField("Players Online:", playersList || "No player information available");
+                        } else if (response.players.online > 0) {
+                            ListEmbed.addField("Players Online:", "Players are online, but names couldn't be retrieved");
+                        } else {
+                            ListEmbed.addField("Players Online:", "No players currently online");
+                        }
+                        
+                        if (response.favicon) {
+                            ListEmbed.setThumbnail("attachment://server-icon.png");
+                        }
+                    } else {
+                        ListEmbed.addField("Status:", "❌ **OFFLINE**");
+                        ListEmbed.setDescription("Server appears to be offline or not responding");
+                    }
 
-            ListEmbed.addField("status: ", output);
-            message.channel.send({ embeds: [ListEmbed] });
-            } catch (status_error) {
-            this.logger.error(status_error.message);
-            const item = server.display_name + " is currently offline!";
-            ListEmbed.addField("status: ", item);
-            message.channel.send({ embeds: [ListEmbed] });
+                } catch (status_error) {
+                    this.logger.error(status_error.message);
+                    ListEmbed.addField("Status:", "❌ **OFFLINE**");
+                    ListEmbed.addField("Error:", `Failed to connect: ${status_error.message}`);
+                    ListEmbed.setDescription("Server appears to be offline or not responding");
+                }
+                
+                const perfTime = ((performance.now() - perfStart) / 1000).toFixed(2);
+                ListEmbed.setFooter(`Response time: ${perfTime} seconds`);
+                
+                message.channel.send({ embeds: [ListEmbed] });
+            } else {
+                message.channel.send({ 
+                    content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers." 
+                });
             }
-        } else {
-            message.channel.send({ content: "Sorry, couldn't find a server with that shortname, try ,listmc for a list of all servers." });
-        }
         } catch (error) {
-        this.logger.error(error);
+            this.logger.error(error);
+            message.channel.send({ 
+                content: `Error getting server information: ${error.message}` 
+            });
         }
     }
 };
