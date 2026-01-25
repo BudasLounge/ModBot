d574cfb51833a6576eccbb15da24c764a1d23228
JR <jrbuda01@gmail.com>
2026-01-25 00:27:32 -0800

    actually working on clash parsing

--- modules/league/fetch_mayhem_augments.js
+++ modules/league/fetch_mayhem_augments.js
@@ -68,7 +68,60 @@ async function fetchAndParseWiki() {
       description = description.replace(/\\"/g, '"');
 
       // Generate Icon Filename
-      const iconName = `${name} augment.png`;
+      let iconName = `${name} augment.png`;
+      
+      // Handle known exceptions for filenames
+      const iconOverrides = {
+        "Escape Plan": "Escape Plan mayhem augment.png",
+        "Adamant": "Adamant mayhem augment.png",
+        "Fetch": "Fetch mayhem augment.png",
+        "Hat on a Hat": "Hat on a Hat mayhem augment.png",
+        "Mighty Shield": "Mighty Shield mayhem augment.png",
+        "Poltergeist": "Poltergeist mayhem augment.png",
+        "ReEnergize": "ReEnergize mayhem augment.png",
+        "Swift and Safe": "Swift and Safe mayhem augment.png",
+        "Transmute: Gold": "Transmute Gold augment.png",
+        "Upgrade Collector": "Upgrade Collector mayhem augment.png",
+        "Upgrade Cutlass": "Upgrade Cutlass mayhem augment.png",
+        "Upgrade Immolate": "Upgrade Immolate mayhem augment.png",
+        "Upgrade Zhonya's": "Upgrade Zhonya's mayhem augment.png",
+        "Wind Beneath Blade": "Wind Beneath Blade mayhem augment.png",
+        "Cheating": "Cheating mayhem augment.png",
+        "Critical Rhythm": "Critical Rhythm mayhem augment.png",
+        "Flash 2": "Flash 2 mayhem augment.png",
+        "Get Excited": "Get Excited mayhem augment.png",
+        "Nightstalking": "Nightstalking mayhem augment.png",
+        "Snowball Upgrade": "Snowball Upgrade mayhem augment.png",
+        "Spiritual Purification": "Spiritual Purification mayhem augment.png",
+        "Transmute: Prismatic": "Transmute Prismatic augment.png",
+        "Upgrade Hubris": "Upgrade Hubris mayhem augment.png",
+        "Upgrade Infinity Edge": "Upgrade Infinity Edge mayhem augment.png",
+        "Upgrade Sheen": "Upgrade Sheen mayhem augment.png",
+        "Vampirism": "Vampirism mayhem augment.png",
+        "I'm a Baby Kitty Where is Mama": "I'm a Baby Kitty Where is Mama mayhem augment.png",
+        "Biggest Snowball Ever": "Biggest Snowball Ever mayhem augment.png",
+        "Cruelty": "Cruelty mayhem augment.png",
+        "Empyrean Promise": "Empyrean Promise mayhem augment.png",
+        "Final Form": "Final Form mayhem augment.png",
+        "Gash": "Gash mayhem augment.png",
+        "Glass Cannon": "Glass Cannon mayhem augment.png",
+        "Goldrend": "Goldrend mayhem augment.png",
+        "King Me": "King Me mayhem augment.png",
+        "Laser Heal": "Laser Heal mayhem augment.png",
+        "Protein Shake": "Protein Shake mayhem augment.png",
+        "Quest: Sneakerhead": "Quest Sneakerhead mayhem augment.png",
+        "Quest: Steel Your Heart": "Quest Steel Your Heart augment.png",
+        "Quest: Urf's Champion": "Quest Urf's Champion augment.png",
+        "Quest: Wooglet's Witchcap": "Quest Wooglet's Witchcap augment.png",
+        "Snowball Roulette": "Snowball Roulette mayhem augment.png",
+        "Transmute: Chaos": "Transmute Chaos augment.png",
+        "Ultimate Awakening": "Ultimate Awakening mayhem augment.png",
+        "Upgrade Mikael's Blessing": "Upgrade Mikael's Blessing mayhem augment.png"
+      };
+
+      if (iconOverrides[name]) {
+        iconName = iconOverrides[name];
+      }
 
       augments[name] = {
         name: name,
